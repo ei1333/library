8c6972054eb91bc95845cc14e128584901d7b2c9
ei1333 <missnakakun@gmail.com>
2019-09-17 21:30:38 +0900

    lazy-segment-tree

--- structure/lazy-segment-tree.cpp
+++ structure/lazy-segment-tree.cpp
@@ -1,10 +1,10 @@
 template< typename Monoid, typename OperatorMonoid = Monoid >
 struct LazySegmentTree {
   using F = function< Monoid(Monoid, Monoid) >;
-  using G = function< Monoid(Monoid, OperatorMonoid, int) >;
+  using G = function< Monoid(Monoid, OperatorMonoid) >;
   using H = function< OperatorMonoid(OperatorMonoid, OperatorMonoid) >;
 
-  int sz;
+  int sz, height;
   vector< Monoid > data;
   vector< OperatorMonoid > lazy;
   const F f;
@@ -18,7 +18,8 @@ struct LazySegmentTree {
                   const Monoid &M1, const OperatorMonoid OM0)
       : f(f), g(g), h(h), M1(M1), OM0(OM0) {
     sz = 1;
-    while(sz < n) sz <<= 1;
+    height = 0;
+    while(sz < n) sz <<= 1, height++;
     data.assign(2 * sz, M1);
     lazy.assign(2 * sz, OM0);
   }
@@ -33,54 +34,101 @@ struct LazySegmentTree {
     }
   }
 
-  void propagate(int k, int len) {
+  inline void propagate(int k) {
     if(lazy[k] != OM0) {
-      if(k < sz) {
-        lazy[2 * k + 0] = h(lazy[2 * k + 0], lazy[k]);
-        lazy[2 * k + 1] = h(lazy[2 * k + 1], lazy[k]);
-      }
-      data[k] = g(data[k], lazy[k], len);
+      lazy[2 * k + 0] = h(lazy[2 * k + 0], lazy[k]);
+      lazy[2 * k + 1] = h(lazy[2 * k + 1], lazy[k]);
+      data[k] = reflect(k);
       lazy[k] = OM0;
     }
   }
 
-  Monoid update(int a, int b, const OperatorMonoid &x, int k, int l, int r) {
-    propagate(k, r - l);
-    if(r <= a || b <= l) {
-      return data[k];
-    } else if(a <= l && r <= b) {
-      lazy[k] = h(lazy[k], x);
-      propagate(k, r - l);
-      return data[k];
-    } else {
-      return data[k] = f(update(a, b, x, 2 * k + 0, l, (l + r) >> 1),
-                         update(a, b, x, 2 * k + 1, (l + r) >> 1, r));
-    }
+  inline Monoid reflect(int k) {
+    return lazy[k] == OM0 ? data[k] : g(data[k], lazy[k]);
+  }
+
+  inline void recalc(int k) {
+    while(k >>= 1) data[k] = f(reflect(2 * k + 0), reflect(2 * k + 1));
+  }
+
+  inline void thrust(int k) {
+    for(int i = height; i > 0; i--) propagate(k >> i);
   }
 
   Monoid update(int a, int b, const OperatorMonoid &x) {
-    return update(a, b, x, 1, 0, sz);
-  }
-
-
-  Monoid query(int a, int b, int k, int l, int r) {
-    propagate(k, r - l);
-    if(r <= a || b <= l) {
-      return M1;
-    } else if(a <= l && r <= b) {
-      return data[k];
-    } else {
-      return f(query(a, b, 2 * k + 0, l, (l + r) >> 1),
-               query(a, b, 2 * k + 1, (l + r) >> 1, r));
+    thrust(a += sz);
+    thrust(b += sz - 1);
+    for(int l = a, r = b + 1; l < r; l >>= 1, r >>= 1) {
+      if(l & 1) lazy[l] = h(lazy[l], x), ++l;
+      if(r & 1) --r, lazy[r] = h(lazy[r], x);
     }
+    recalc(a);
+    recalc(b);
   }
 
   Monoid query(int a, int b) {
-    return query(a, b, 1, 0, sz);
+    thrust(a += sz);
+    thrust(b += sz - 1);
+    Monoid L = M1, R = M1;
+    for(int l = a, r = b + 1; l < r; l >>= 1, r >>= 1) {
+      if(l & 1) L = f(L, reflect(l++));
+      if(r & 1) R = f(reflect(--r), R);
+    }
+    return f(L, R);
   }
 
   Monoid operator[](const int &k) {
     return query(k, k + 1);
   }
-};
 
+  template< typename C >
+  int find_subtree(int a, const C &check, Monoid &M, bool type) {
+    while(a < sz) {
+      propagate(a);
+      Monoid nxt = type ? f(reflect(2 * a + type), M) : f(M, reflect(2 * a + type));
+      if(check(nxt)) a = 2 * a + type;
+      else M = nxt, a = 2 * a + 1 - type;
+    }
+    return a - sz;
+  }
+
+  template< typename C >
+  int find_first(int a, const C &check) {
+    Monoid L = M1;
+    if(a <= 0) {
+      if(check(f(L, reflect(1)))) return find_subtree(1, check, L, false);
+      return -1;
+    }
+    thrust(a + sz);
+    int b = sz;
+    for(a += sz, b += sz; a < b; a >>= 1, b >>= 1) {
+      if(a & 1) {
+        Monoid nxt = f(L, reflect(a));
+        if(check(nxt)) return find_subtree(a, check, L, false);
+        L = nxt;
+        ++a;
+      }
+    }
+    return -1;
+  }
+
+
+  template< typename C >
+  int find_last(int b, const C &check) {
+    Monoid R = M1;
+    if(b >= sz) {
+      if(check(f(reflect(1), R))) return find_subtree(1, check, R, true);
+      return -1;
+    }
+    thrust(b + sz - 1);
+    int a = sz;
+    for(b += sz; a < b; a >>= 1, b >>= 1) {
+      if(b & 1) {
+        Monoid nxt = f(reflect(--b), R);
+        if(check(nxt)) return find_subtree(b, check, R, true);
+        R = nxt;
+      }
+    }
+    return -1;
+  }
+};
