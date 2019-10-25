dff554e1a4dd98debba6ff91f9378474067c2294
ei1333 <missnakakun@gmail.com>
2019-10-25 20:21:09 +0900

    fix mod-log

--- math/mod-log.cpp
+++ math/mod-log.cpp
@@ -1,23 +1,32 @@
 int64_t mod_log(int64_t a, int64_t b, int64_t p) {
-  int64_t ok = p, ng = -1;
-  while(ok - ng > 1) {
-    auto mid = (ok + ng) / 2;
-    if(mid * mid >= p) ok = mid;
-    else ng = mid;
+  int64_t g = 1;
+
+  for(int64_t i = p; i; i /= 2) (g *= a) %= p;
+  g = __gcd(g, p);
+
+  int64_t t = 1, c = 0;
+  for(; t % g; c++) {
+    if(t == b) return c;
+    (t *= a) %= p;
   }
- 
-  unordered_map< int64_t, int64_t > baby;
-  baby.reserve(ok);
-  int64_t factor = 1;
-  for(int64_t i = 0, e = b; i < ok; i++) {
-    baby[e] = i;
-    (factor *= a) %= p;
-    (e *= a) %= p;
+  if(b % g) return -1;
+
+  t /= g;
+  b /= g;
+
+  int64_t n = p / g, h = 0, gs = 1;
+
+  for(; h * h < n; h++) (gs *= a) %= n;
+
+  unordered_map< int64_t, int64_t > bs;
+  for(int64_t s = 0, e = b; s < h; bs[e] = ++s) {
+    (e *= a) %= n;
   }
-  for(int64_t i = 1, e = factor; i <= ok; i++) {
-    auto it = baby.find(e);
-    if(it != end(baby)) return i * ok - it->second;
-    (e *= factor) %= p;
+
+  for(int64_t s = 0, e = t; s < n;) {
+    (e *= gs) %= n;
+    s += h;
+    if(bs.count(e)) return c + s - bs[e];
   }
   return -1;
 }
