45451279e8969288f72e06da50fc5118a11863ef
ei1333 <missnakakun@gmail.com>
2019-11-16 02:02:49 +0900

    push-relabel fix

--- graph/push-relabel.cpp
+++ graph/push-relabel.cpp
@@ -1,7 +1,5 @@
 template< typename flow_t >
 struct PushRelabel {
-  const flow_t INF;
-
   struct edge {
     int to;
     flow_t cap;
@@ -9,113 +7,142 @@ struct PushRelabel {
     bool isrev;
     int idx;
   };
+
   vector< vector< edge > > graph;
+  vector< int > potential, cur_edge;
   vector< flow_t > ex;
-  int relabels, high;
-  vector< int > cnt, h;
-  vector< vector< int > > hs;
+  int V, height, relabels;
+  List all_ver;
+  Stack act_ver;
 
-  PushRelabel(int V) : graph(V), INF(numeric_limits< flow_t >::max()), hs(V + 1), high(0) {}
-
+  PushRelabel(int V)
+      : V(V), height(-1), relabels(0), ex(V, flow_t(0)), potential(V, 0), cur_edge(V), all_ver(V, V), act_ver(V, V), graph(V) {}
 
   void add_edge(int from, int to, flow_t cap, int idx = -1) {
     graph[from].emplace_back((edge) {to, cap, (int) graph[to].size(), false, idx});
     graph[to].emplace_back((edge) {from, 0, (int) graph[from].size() - 1, true, idx});
   }
 
-  void update_height(int idx, int nxt_height) {
-    ++relabels;
-    if(h[idx] != graph.size() + 1) {
-      --cnt[h[idx]];
+  int calc_active(int t) {
+    height = -1;
+    for(int i = 0; i < V; i++) {
+      if(potential[i] < V) {
+        cur_edge[i] = 0;
+        height = max(height, potential[i]);
+        all_ver.insert(potential[i], i);
+        if(ex[i] > 0 && i != t) act_ver.push(potential[i], i);
+      } else {
+        potential[i] = V + 1;
+      }
     }
-    h[idx] = nxt_height;
-    if(h[idx] != graph.size() + 1) {
-      high = nxt_height;
-      ++cnt[nxt_height];
-      if(ex[idx] > 0) hs[nxt_height].emplace_back(idx);
-    }
+    return height;
   }
 
-  void global_relabel(int idx) {
-    for(int i = 0; i <= high; i++) hs[i].clear();
-    relabels = 0;
-    high = 0;
-    h.assign(graph.size(), graph.size() + 1);
-    cnt.assign(graph.size(), 0);
+  void bfs(int t) {
+    for(int i = 0; i < V; i++) {
+      potential[i] = max(potential[i], V);
+    }
+    potential[t] = 0;
     queue< int > que;
-    que.emplace(idx);
-    h[idx] = 0;
-    while(que.size()) {
+    que.emplace(t);
+    while(!que.empty()) {
       int p = que.front();
       que.pop();
       for(auto &e : graph[p]) {
-        if(h[e.to] == graph.size() + 1 && graph[e.to][e.rev].cap > 0) {
+        if(potential[e.to] == V && graph[e.to][e.rev].cap > 0) {
+          potential[e.to] = potential[p] + 1;
           que.emplace(e.to);
-          high = h[p] + 1;
-          update_height(e.to, high);
         }
       }
     }
   }
 
-
-  void push(int idx, edge &e) {
-    if(h[e.to] == graph.size() + 1) return;
-    if(ex[e.to] == 0) {
-      hs[h[e.to]].emplace_back(e.to);
+  int init(int s, int t) {
+    potential[s] = V + 1;
+    bfs(t);
+    for(auto &e : graph[s]) {
+      if(potential[e.to] < V) {
+        graph[e.to][e.rev].cap = e.cap;
+        ex[s] -= e.cap;
+        ex[e.to] += e.cap;
+      }
+      e.cap = 0;
     }
-    flow_t df = min(ex[idx], e.cap);
-    e.cap -= df;
-    graph[e.to][e.rev].cap += df;
-    ex[idx] -= df;
-    ex[e.to] += df;
+    return calc_active(t);
   }
 
-  void discharge(int idx) {
-    int next_height = (int) graph.size() + 1;
-    for(auto &&e : graph[idx]) {
-      if(e.cap > 0) {
-        if(h[idx] == h[e.to] + 1) {
-          push(idx, e);
-          if(ex[idx] <= 0) return;
-        } else {
-          next_height = min(next_height, h[e.to] + 1);
-        }
+  bool push(int u, int t, edge &e) {
+    flow_t f = min(e.cap, ex[u]);
+    int v = e.to;
+    e.cap -= f, ex[u] -= f;
+    graph[v][e.rev].cap += f, ex[v] += f;
+    if(ex[v] == f && v != t) act_ver.push(potential[v], v);
+    return ex[u] == 0;
+  }
+
+  int discharge(int u, int t) {
+    for(int &i = cur_edge[u]; i < graph[u].size(); i++) {
+      auto &e = graph[u][i];
+      if(potential[u] == potential[e.to] + 1 && e.cap > 0) {
+        if(push(u, t, e)) return potential[u];
       }
     }
-    if(cnt[h[idx]] > 1) {
-      update_height(idx, next_height);
+    return relabel(u);
+  }
+
+  int global_relabel(int t) {
+    bfs(t);
+    all_ver.clear(), act_ver.clear();
+    return calc_active(t);
+  }
+
+  void gap_relabel(const int u) {
+    for(int i = potential[u]; i <= height; ++i) {
+      for(int id = all_ver.dat[V + i].next; id < V; id = all_ver.dat[id].next) {
+        potential[id] = V + 1;
+      }
+      all_ver.dat[V + i].prev = all_ver.dat[V + i].next = V + i;
+    }
+  }
+
+  int relabel(const int u) {
+    ++relabels;
+    int prv = potential[u], cur = V;
+    for(int i = 0; i < (int) graph[u].size(); ++i) {
+      const edge &e = graph[u][i];
+      if(cur > potential[e.to] + 1 && e.cap > 0) {
+        cur_edge[u] = i;
+        cur = potential[e.to] + 1;
+      }
+    }
+    if(all_ver.more_one(prv)) {
+      all_ver.erase(u);
+      if((potential[u] = cur) == V) return potential[u] = V + 1, prv;
+      act_ver.push(cur, u);
+      all_ver.insert(cur, u);
+      height = max(height, cur);
     } else {
-      for(; high >= h[idx]; hs[high--].clear()) {
-        for(int j : hs[high]) update_height(j, graph.size() + 1);
-      }
+      gap_relabel(u);
+      return height = prv - 1;
     }
+    return cur;
   }
 
   flow_t max_flow(int s, int t) {
-    ex.assign(graph.size(), 0);
-    ex[s] = INF;
-    ex[t] = -INF;
-    global_relabel(t);
-    for(auto &e : graph[s]) push(s, e);
-    for(; high >= 0; high--) {
-      while(!hs[high].empty()) {
-        int idx = hs[high].back();
-        hs[high].pop_back();
-        discharge(idx);
-        if(relabels >= graph.size() * 4) global_relabel(t);
+    int level = init(s, t);
+    while(level >= 0) {
+      if(act_ver.empty(level)) {
+        --level;
+        continue;
+      }
+      int u = act_ver.top(level);
+      act_ver.pop(level);
+      level = discharge(u, t);
+      if(relabels * 2 >= V) {
+        level = global_relabel(t);
+        relabels = 0;
       }
     }
-    return ex[t] + INF;
-  }
-
-  void output() {
-    for(int i = 0; i < graph.size(); i++) {
-      for(auto &e : graph[i]) {
-        if(e.isrev) continue;
-        auto &rev_e = graph[e.to][e.rev];
-        cout << i << "->" << e.to << " (flow: " << rev_e.cap << "/" << e.cap + rev_e.cap << ")" << endl;
-      }
-    }
+    return ex[t];
   }
 };
