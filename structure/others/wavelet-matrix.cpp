2ee05c158b055ecbc6fd2932a459ee07d4e816af
ei1333 <missnakakun@gmail.com>
2019-12-27 01:02:34 +0900

    fix wavelet-matrix

--- structure/others/wavelet-matrix.cpp
+++ structure/others/wavelet-matrix.cpp
@@ -3,12 +3,9 @@ struct SuccinctIndexableDictionary {
   size_t blocks;
   vector< unsigned > bit, sum;
 
-  SuccinctIndexableDictionary() {
-  }
+  SuccinctIndexableDictionary() = default;
 
-  SuccinctIndexableDictionary(size_t _length) {
-    length = _length;
-    blocks = (length + 31) >> 5;
+  SuccinctIndexableDictionary(size_t length) : length(length), blocks((length + 31) >> 5) {
     bit.assign(blocks, 0U);
     sum.assign(blocks, 0U);
   }
@@ -24,7 +21,7 @@ struct SuccinctIndexableDictionary {
     }
   }
 
-  bool operator[](int k) const {
+  bool operator[](int k) {
     return (bool((bit[k >> 5] >> (k & 31)) & 1));
   }
 
@@ -35,117 +32,101 @@ struct SuccinctIndexableDictionary {
   int rank(bool val, int k) {
     return (val ? rank(k) : k - rank(k));
   }
-
-  int select(bool val, int k) {
-    if(k < 0 || rank(val, length) <= k) return (-1);
-    int low = 0, high = length;
-    while(high - low > 1) {
-      int mid = (low + high) >> 1;
-      if(rank(val, mid) >= k + 1) high = mid;
-      else low = mid;
-    }
-    return (high - 1);
-  }
-
-  int select(bool val, int i, int l) {
-    return select(val, i + rank(val, l));
-  }
 };
 
-template< class T, int MAXLOG >
+
+template< typename T, int MAXLOG >
 struct WaveletMatrix {
   size_t length;
   SuccinctIndexableDictionary matrix[MAXLOG];
-  int zs[MAXLOG];
-  int buff1[MAXLOG], buff2[MAXLOG];
+  int mid[MAXLOG];
 
-  int freq_dfs(int d, int l, int r, T val, T a, T b) {
-    if(l == r) return 0;
-    if(d == MAXLOG) return (a <= val && val < b) ? r - l : 0;
-    T nv = 1ULL << (MAXLOG - d - 1) | val, nnv = ((1ULL << (MAXLOG - d - 1)) - 1) | nv;
-    if(nnv < a || b <= val) return 0;
-    if(a <= val && nnv < b) return r - l;
-    int lc = matrix[d].rank(1, l), rc = matrix[d].rank(1, r);
-    return freq_dfs(d + 1, l - lc, r - rc, val, a, b) +
-           freq_dfs(d + 1, lc + zs[d], rc + zs[d], nv, a, b);
-  }
-
-  WaveletMatrix(vector< T > data) {
-    length = data.size();
+  WaveletMatrix(vector< T > v) : length(v.size()) {
     vector< T > l(length), r(length);
-    for(int depth = 0; depth < MAXLOG; depth++) {
-      matrix[depth] = SuccinctIndexableDictionary(length + 1);
+    for(int level = MAXLOG - 1; level >= 0; level--) {
+      matrix[level] = SuccinctIndexableDictionary(length + 1);
       int left = 0, right = 0;
       for(int i = 0; i < length; i++) {
-        bool k = (data[i] >> (MAXLOG - depth - 1)) & 1;
-        if(k) r[right++] = data[i], matrix[depth].set(i);
-        else l[left++] = data[i];
+        if(((v[i] >> level) & 1)) {
+          matrix[level].set(i);
+          r[right++] = v[i];
+        } else {
+          l[left++] = v[i];
+        }
       }
-      zs[depth] = left;
-      matrix[depth].build();
-      swap(l, data);
-      for(int i = 0; i < right; i++) data[left + i] = r[i];
+      mid[level] = left;
+      matrix[level].build();
+      v.swap(l);
+      for(int i = 0; i < right; i++) {
+        v[left + i] = r[i];
+      }
     }
   }
 
+  pair< int, int > succ(bool f, int l, int r, int level) {
+    return {matrix[level].rank(f, l) + mid[level] * f, matrix[level].rank(f, r) + mid[level] * f};
+  }
+
+  // v[k]
   T access(int k) {
-    int ret = 0;
-    bool bit;
-    for(int depth = 0; depth < MAXLOG; depth++) {
-      bit = matrix[depth][k];
-      ret = (ret << 1) | bit;
-      k = matrix[depth].rank(bit, k) + zs[depth] * bit;
-    }
-    return (ret);
-  }
-
-  int rank(T val, int k) {
-    int l = 0, r = k;
-    for(int depth = 0; depth < MAXLOG; depth++) {
-      buff1[depth] = l, buff2[depth] = r;
-      bool bit = (val >> (MAXLOG - depth - 1)) & 1;
-      l = matrix[depth].rank(bit, l) + zs[depth] * bit;
-      r = matrix[depth].rank(bit, r) + zs[depth] * bit;
-    }
-    return (r - l);
-  }
-
-  int select(T val, int kth) {
-    rank(val, length);
-    for(int depth = MAXLOG - 1; depth >= 0; depth--) {
-      bool bit = (val >> (MAXLOG - depth - 1)) & 1;
-      kth = matrix[depth].select(bit, kth, buff1[depth]);
-      if(kth >= buff2[depth] || kth < 0) return (-1);
-      kth -= buff1[depth];
-    }
-    return (kth);
-  }
-
-  int select(T val, int k, int l) {
-    return select(val, k + rank(val, l));
-  }
-
-  int quantile(int left, int right, int kth) {
-    if(right - left <= kth || kth < 0) return (-1);
     T ret = 0;
-    for(int depth = 0; depth < MAXLOG; depth++) {
-      int l = matrix[depth].rank(1, left);
-      int r = matrix[depth].rank(1, right);
-      if(r - l > kth) {
-        left = l + zs[depth];
-        right = r + zs[depth];
-        ret |= 1ULL << (MAXLOG - depth - 1);
-      } else {
-        kth -= r - l;
-        left -= l;
-        right -= r;
-      }
+    for(int level = MAXLOG - 1; level >= 0; level--) {
+      bool f = matrix[level][k];
+      if(f) ret |= T(1) << level;
+      k = matrix[level].rank(f, k) + mid[level] * f;
     }
     return ret;
   }
-  
-  int rangefreq(int left, int right, T lower, T upper) {
-    return freq_dfs(0, left, right, 0, lower, upper);
+
+  T operator[](const int &k) {
+    return access(k);
+  }
+
+  // count i s.t. (0 <= i < r) && v[i] == x
+  int rank(const T &x, int r) {
+    int l = 0;
+    for(int level = MAXLOG - 1; level >= 0; level--) {
+      tie(l, r) = succ((x >> level) & 1, l, r, level);
+    }
+    return r - l;
+  }
+
+
+  // k-th(0-indexed) smallest number in v[l,r)
+  T kth_smallest(int l, int r, int k) {
+    assert(0 <= k && k < r - l);
+    T ret = 0;
+    for(int level = MAXLOG - 1; level >= 0; level--) {
+      int cnt = matrix[level].rank(false, r) - matrix[level].rank(false, l);
+      bool f = cnt <= k;
+      if(f) {
+        ret |= T(1) << level;
+        k -= cnt;
+      }
+      tie(l, r) = succ(f, l, r, level);
+    }
+    return ret;
+  }
+
+  // k-th(0-indexed) largest number in v[l,r)
+  T kth_largest(int l, int r, int k) {
+    return kth_smallest(l, r, r - l - k - 1);
+  }
+
+  // count i s.t. (l <= i < r) && (v[i] < upper)
+  T range_freq(int l, int r, T upper) {
+    int ret = 0;
+    for(int level = MAXLOG - 1; level >= 0; level--) {
+      bool f = ((upper >> level) & 1);
+      if(f) ret += matrix[level].rank(false, r) - matrix[level].rank(false, l);
+      tie(l, r) = succ(f, l, r, level);
+    }
+    return ret;
+  }
+
+  // count i s.t. (l <= i < r) && (lower <= v[i] < upper)
+  T range_freq(int l, int r, T lower, T upper) {
+    return range_freq(l, r, upper) - range_freq(l, r, lower);
   }
 };
 
---
+++ test/verify/aoj-2674.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2674"
+
+#include "../../template/template.cpp"
+
+#include "../../structure/others/wavelet-matrix.cpp"
+
+int main() {
+  int N, Q;
+  cin >> N;
+  vector< int > X(N);
+  for(int i = 0; i < N; i++) {
+    cin >> X[i];
+    X[i] += 5e8;
+  }
+  WaveletMatrix< int, 30 > matrix(X);
+  cin >> Q;
+  while(Q--) {
+    int L, R, E;
+    cin >> L >> R >> E;
+    --L, --R;
+    cout << (R - L + 1) - matrix.range_freq(L, R + 1, min(X[L], X[R]) - E, max(X[L], X[R]) + E + 1) << "\n";
+  }
+}
