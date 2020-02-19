d4fb490476ec1d7b9ac1f5aac0561648558e4a05
ei1333 <missnakakun@gmail.com>
2020-02-19 21:28:15 +0900

    test

--- structure/union-find/union-find.cpp
+++ structure/union-find/union-find.cpp
@@ -1,25 +1,30 @@
+/* @docs docs/union-find.md */
 struct UnionFind {
   vector< int > data;
- 
-  UnionFind(int sz) {
-    data.assign(sz, -1);
-  }
- 
+
+  UnionFind() = default;
+
+  explicit UnionFind(size_t sz) : data(sz, -1) {}
+  
   bool unite(int x, int y) {
     x = find(x), y = find(y);
-    if(x == y) return (false);
+    if(x == y) return false;
     if(data[x] > data[y]) swap(x, y);
     data[x] += data[y];
     data[y] = x;
-    return (true);
+    return true;
   }
- 
+
   int find(int k) {
     if(data[k] < 0) return (k);
-    return (data[k] = find(data[k]));
+    return data[k] = find(data[k]);
   }
- 
+
   int size(int k) {
-    return (-data[find(k)]);
+    return -data[find(k)];
+  }
+
+  bool is_same(int x, int y) {
+    return find(x) == find(y);
   }
 };
