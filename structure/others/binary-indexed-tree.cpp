232ff3e44184b5bdd0ac4d05813fe39b65b3fbb0
ei1333 <missnakakun@gmail.com>
2020-02-19 22:38:55 +0900

    bit

--- structure/others/binary-indexed-tree.cpp
+++ structure/others/binary-indexed-tree.cpp
@@ -1,18 +1,44 @@
+/**
+ * @brief Binary-Indexed-Tree(BIT)
+ * @docs docs/binary-indexed-tree.md
+ */
 template< typename T >
 struct BinaryIndexedTree {
   vector< T > data;
 
-  BinaryIndexedTree(int sz) {
-    data.assign(++sz, 0);
+  BinaryIndexedTree() = default;
+
+  explicit BinaryIndexedTree(size_t sz) : data(sz + 1, 0) {}
+
+  void add(int k, const T &x) {
+    for(++k; k < (int) data.size(); k += k & -k) data[k] += x;
   }
 
-  T sum(int k) {
-    T ret = 0;
+  T query(int k) const {
+    T ret = T();
     for(++k; k > 0; k -= k & -k) ret += data[k];
-    return (ret);
+    return ret;
   }
 
-  void add(int k, T x) {
-    for(++k; k < data.size(); k += k & -k) data[k] += x;
+  int lower_bound(T x) const {
+    int i = 0;
+    for(int k = 1 << (__lg(data.size() - 1) + 1); k > 0; k >>= 1) {
+      if(i + k < data.size() && data[i + k] < x) {
+        x -= data[i + k];
+        i += k;
+      }
+    }
+    return i;
+  }
+
+  int upper_bound(T x) const {
+    int i = 0;
+    for(int k = 1 << (__lg(data.size() - 1) + 1); k > 0; k >>= 1) {
+      if(i + k < data.size() && data[i + k] <= x) {
+        x -= data[i + k];
+        i += k;
+      }
+    }
+    return i;
   }
 };
--- test/verify/aoj-dsl-2-b.test.cpp
+++ test/verify/aoj-dsl-2-b.test.cpp
@@ -6,12 +6,12 @@
 
 int main() {
   int N, Q;
-  scanf("%d %d", &N, &Q);
+  cin >> N >> Q;
   BinaryIndexedTree< int > bit(N);
   while(Q--) {
     int T, X, Y;
-    scanf("%d %d %d", &T, &X, &Y);
+    cin >> T >> X >> Y;
     if(T == 0) bit.add(X - 1, Y);
-    else printf("%d\n", bit.sum(Y - 1) - bit.sum(X - 2));
+    else cout << bit.query(Y - 1) - bit.query(X - 2) << "\n";
   }
 }
