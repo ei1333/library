11dc1a016fe79ad04088b91dc05b3a7b7af342cf
ei13333 <missnakakun@gmail.com>
2020-05-27 13:26:09 +0900

    tree-decomp

---
+++ test/verify/yosupo-tree-decomposition-width-2.test.cpp
@@ -0,0 +1,53 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/tree_decomposition_width_2"
+
+#include "../../template/template.cpp"
+
+#include "../../other/scanner.cpp"
+#include "../../other/printer.cpp"
+
+#include "../../structure/union-find/union-find.cpp"
+
+#include "../../graph/others/tree-decomposition.cpp"
+
+int main() {
+  string x;
+  int N, M;
+  Scanner pin(stdin);
+  Printer pout(stdout);
+  pin.read(x, x, N, M);
+  vector< int > A(M), B(M);
+  UnionFind uf(N);
+  for(int i = 0; i < M; i++) {
+    pin.read(A[i], B[i]);
+    --A[i], --B[i];
+    uf.unite(A[i], B[i]);
+  }
+  int root = -1;
+  for(int i = 0; i < N; i++) {
+    if(uf.find(i) == i) {
+      if(root == -1) {
+        root = i;
+      } else {
+        A.emplace_back(root);
+        B.emplace_back(i);
+      }
+    }
+  }
+  TreeDecomposition td(N);
+  for(int i = 0; i < A.size(); i++) {
+    td.add_edge(A[i], B[i]);
+  }
+  auto tap = td.build();
+  if(tap.empty()) {
+    pout.writeln("-1");
+    return 0;
+  }
+  pout.writeln("s", "td", tap.size(), 2, N);
+  for(int i = 0; i < tap.size(); i++) {
+    for(auto &t : tap[i].bag) ++t;
+    pout.writeln("b", i + 1, tap[i].bag);
+  }
+  for(int i = 0; i < tap.size(); i++) {
+    for(auto &t : tap[i].child) pout.writeln(i + 1, t + 1);
+  }
+}
