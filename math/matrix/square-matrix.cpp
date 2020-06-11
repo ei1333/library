7d8d02496c982ebc2287456269c59889557d21af
ei13333 <missnakakun@gmail.com>
2020-06-11 22:00:32 +0900

    add square-matrix

---
+++ math/matrix/square-matrix.cpp
@@ -0,0 +1,97 @@
+/**
+ * @brief Square-Matrix(正方行列)
+ */
+template< class T, size_t N >
+struct SquareMatrix {
+  array< array< T, N >, N > A;
+
+  SquareMatrix() = default;
+
+  size_t size() { return N; }
+
+  inline const array< T, N > &operator[](int k) const {
+    return (A.at(k));
+  }
+
+  inline array< T, N > &operator[](int k) {
+    return (A.at(k));
+  }
+
+  static SquareMatrix add_identity() {
+    return SquareMatrix();
+  }
+
+  static SquareMatrix mul_identity() {
+    SquareMatrix mat;
+    for(size_t i = 0; i < N; i++) mat[i][i] = 1;
+    return mat;
+  }
+
+  SquareMatrix &operator+=(const SquareMatrix &B) {
+    for(size_t i = 0; i < N; i++) {
+      for(size_t j = 0; j < N; j++) {
+        (*this)[i][j] += B[i][j];
+      }
+    }
+    return *this;
+  }
+
+  SquareMatrix &operator-=(const SquareMatrix &B) {
+    for(size_t i = 0; i < N; i++) {
+      for(size_t j = 0; j < N; j++) {
+        (*this)[i][j] -= B[i][j];
+      }
+    }
+    return *this;
+  }
+
+  SquareMatrix &operator*=(const SquareMatrix &B) {
+    array< array< T, N >, N > C;
+    for(size_t i = 0; i < N; i++) {
+      for(size_t j = 0; j < N; j++) {
+        for(size_t k = 0; k < N; k++) {
+          C[i][j] = (C[i][j] + (*this)[i][k] * B[k][j]);
+        }
+      }
+    }
+    A.swap(C);
+    return (*this);
+  }
+
+  SquareMatrix &operator^=(uint64_t k) {
+    SquareMatrix B = SquareMatrix::mul_identity();
+    while(k > 0) {
+      if(k & 1) B *= *this;
+      *this *= *this;
+      k >>= 1LL;
+    }
+    A.swap(B.A);
+    return *this;
+  }
+
+  SquareMatrix operator+(const SquareMatrix &B) const {
+    return SquareMatrix(*this) += B;
+  }
+
+  SquareMatrix operator-(const SquareMatrix &B) const {
+    return SquareMatrix(*this) -= B;
+  }
+
+  SquareMatrix operator*(const SquareMatrix &B) const {
+    return SquareMatrix(*this) *= B;
+  }
+
+  SquareMatrix operator^(uint64_t k) const {
+    return SquareMatrix(*this) ^= k;
+  }
+
+  friend ostream &operator<<(ostream &os, SquareMatrix &p) {
+    for(int i = 0; i < N; i++) {
+      os << "[";
+      for(int j = 0; j < N; j++) {
+        os << p[i][j] << (j + 1 == N ? "]\n" : ",");
+      }
+    }
+    return os;
+  }
+};
---
+++ test/verify/yukicoder-650.test.cpp
@@ -0,0 +1,47 @@
+#define PROBLEM "https://yukicoder.me/problems/no/650"
+
+#include "../../template/template.cpp"
+
+#include "../../graph/graph-template.cpp"
+#include "../../graph/tree/heavy-light-decomposition.cpp"
+
+#include "../../structure/segment-tree/segment-tree.cpp"
+
+#include "../../math/combinatorics/mod-int.cpp"
+#include "../../math/matrix/square-matrix.cpp"
+
+int main() {
+  int N;
+  cin >> N;
+  vector< int > X(N), Y(N);
+  HeavyLightDecomposition<> g(N);
+  for(int i = 1; i < N; i++) {
+    cin >> X[i] >> Y[i];
+    g.add_edge(X[i], Y[i]);
+  }
+  g.build();
+  for(int i = 1; i < N; i++) {
+    if(g.in[X[i]] > g.in[Y[i]]) swap(X[i], Y[i]);
+  }
+  using Mat = SquareMatrix< modint, 2 >;
+  auto f = [](const Mat &a, const Mat &b) { return a * b; };
+  SegmentTree< Mat > seg(N, f, Mat::mul_identity());
+  int Q;
+  cin >> Q;
+  while(Q--) {
+    char x;
+    cin >> x;
+    if(x == 'x') {
+      int v;
+      cin >> v;
+      Mat m;
+      cin >> m[0][0] >> m[0][1] >> m[1][0] >> m[1][1];
+      seg.update(g.in[Y[v + 1]], m);
+    } else {
+      int y, z;
+      cin >> y >> z;
+      auto mat = g.query(y, z, Mat::mul_identity(), [&](int a, int b) { return seg.query(a, b); }, f, true);
+      cout << mat[0][0] << " " << mat[0][1] << " " << mat[1][0] << " " << mat[1][1] << "\n";
+    }
+  }
+}
