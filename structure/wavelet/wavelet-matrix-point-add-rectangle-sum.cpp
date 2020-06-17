0d3f8a52abd7105c5394399f5bd4cd36162a778f
ei13333 <missnakakun@gmail.com>
2020-06-17 14:58:48 +0900

    wm-point-add-rect-sum

---
+++ structure/wavelet/wavelet-matrix-point-add-rectangle-sum.cpp
@@ -0,0 +1,95 @@
+template< typename T, int MAXLOG, typename D >
+struct WaveletMatrixPointAddRectangleSum {
+  size_t length;
+  SuccinctIndexableDictionary matrix[MAXLOG];
+  BinaryIndexedTree< D > ds[MAXLOG];
+  int mid[MAXLOG];
+
+  WaveletMatrixPointAddRectangleSum() = default;
+
+  WaveletMatrixPointAddRectangleSum(const vector< T > &v, const vector< D > &d) : length(v.size()) {
+    assert(v.size() == d.size());
+    vector< int > l(length), r(length), ord(length);
+    iota(begin(ord), end(ord), 0);
+    vector< D > dd(length);
+    for(int level = MAXLOG - 1; level >= 0; level--) {
+      matrix[level] = SuccinctIndexableDictionary(length + 1);
+      int left = 0, right = 0;
+      for(int i = 0; i < length; i++) {
+        if(((v[ord[i]] >> level) & 1)) {
+          matrix[level].set(i);
+          r[right++] = ord[i];
+        } else {
+          l[left++] = ord[i];
+        }
+      }
+      mid[level] = left;
+      matrix[level].build();
+      ord.swap(l);
+      for(int i = 0; i < right; i++) {
+        ord[left + i] = r[i];
+      }
+      for(int i = 0; i < length; i++) {
+        dd[i] = d[ord[i]];
+      }
+      ds[level] = BinaryIndexedTree< D >(dd);
+    }
+  }
+
+  pair< int, int > succ(bool f, int l, int r, int level) {
+    return {matrix[level].rank(f, l) + mid[level] * f, matrix[level].rank(f, r) + mid[level] * f};
+  }
+
+  // count d[i] s.t. (l <= i < r) && (v[i] < upper)
+  D rect_sum(int l, int r, T upper) {
+    D ret = 0;
+    for(int level = MAXLOG - 1; level >= 0; level--) {
+      bool f = ((upper >> level) & 1);
+      if(f) ret += ds[level].query(matrix[level].rank(false, r) - 1) - ds[level].query(matrix[level].rank(false, l) - 1);
+      tie(l, r) = succ(f, l, r, level);
+    }
+    return ret;
+  }
+
+  D rect_sum(int l, int r, T lower, T upper) {
+    return rect_sum(l, r, upper) - rect_sum(l, r, lower);
+  }
+
+  void point_add(int k, T y, const D &x) {
+    for(int level = MAXLOG - 1; level >= 0; level--) {
+      bool f = ((y >> level) & 1);
+      k = matrix[level].rank(f, k) + mid[level] * f;
+      ds[level].add(k, x);
+    }
+  }
+};
+
+template< typename T, int MAXLOG, typename D >
+struct CompressedWaveletMatrixPointAddRectangleSum {
+  WaveletMatrixPointAddRectangleSum< int, MAXLOG, D > mat;
+  vector< T > ys;
+
+  CompressedWaveletMatrixPointAddRectangleSum(const vector< T > &v, const vector< D > &d) : ys(v) {
+    sort(begin(ys), end(ys));
+    ys.erase(unique(begin(ys), end(ys)), end(ys));
+    vector< int > t(v.size());
+    for(int i = 0; i < v.size(); i++) t[i] = get(v[i]);
+    mat = WaveletMatrixPointAddRectangleSum< int, MAXLOG, D >(t, d);
+  }
+
+  inline int get(const T &x) {
+    return lower_bound(begin(ys), end(ys), x) - begin(ys);
+  }
+
+  D rect_sum(int l, int r, T upper) {
+    return mat.rect_sum(l, r, get(upper));
+  }
+
+  D rect_sum(int l, int r, T lower, T upper) {
+    return mat.rect_sum(l, r, get(lower), get(upper));
+  }
+
+  void point_add(int k, T y, const D &x) {
+    mat.point_add(k, get(y), x);
+  }
+};
---
+++ test/verify/yosupo-point-add-rectangle-sum.test.cpp
@@ -0,0 +1,56 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_add_rectangle_sum"
+
+#include "../../template/template.cpp"
+
+#include "../../structure/others/binary-indexed-tree.cpp"
+
+#include "../../structure/wavelet/succinct-indexable-dictionary.cpp"
+#include "../../structure/wavelet/wavelet-matrix-point-add-rectangle-sum.cpp"
+
+#include "../../other/scanner.cpp"
+#include "../../other/printer.cpp"
+
+int main() {
+  Scanner in(stdin);
+  Printer out(stdout);
+  int N, Q;
+  in.read(N, Q);
+  vector< int > x(N), y(N), w(N);
+  vector< pair< int, int > > xs;
+  xs.reserve(N + Q);
+  for(int i = 0; i < N; i++) {
+    in.read(x[i], y[i], w[i]);
+    xs.emplace_back(x[i], y[i]);
+  }
+  vector< int > t(Q), a(Q), b(Q), c(Q), d(Q);
+  for(int i = 0; i < Q; i++) {
+    in.read(t[i], a[i], b[i], c[i]);
+    if(t[i] == 0) xs.emplace_back(a[i], b[i]);
+    else in.read(d[i]);
+  }
+  sort(begin(xs), end(xs));
+  xs.erase(unique(begin(xs), end(xs)), end(xs));
+  vector< int > ys(xs.size());
+  for(int i = 0; i < xs.size(); i++) ys[i] = xs[i].second;
+  vector< int64 > ws(xs.size());
+  for(int i = 0; i < N; i++) {
+    x[i] = lower_bound(begin(xs), end(xs), make_pair(x[i], y[i])) - begin(xs);
+    ws[x[i]] += w[i];
+  }
+  for(int i = 0; i < Q; i++) {
+    if(t[i] == 0) {
+      a[i] = lower_bound(begin(xs), end(xs), make_pair(a[i], b[i])) - begin(xs);
+    } else {
+      a[i] = lower_bound(begin(xs), end(xs), make_pair(a[i], -1)) - begin(xs);
+      c[i] = lower_bound(begin(xs), end(xs), make_pair(c[i], -1)) - begin(xs);
+    }
+  }
+  CompressedWaveletMatrixPointAddRectangleSum< int, 18, int64 > mat(ys, ws);
+  for(int i = 0; i < Q; i++) {
+    if(t[i] == 0) {
+      mat.point_add(a[i], b[i], c[i]);
+    } else {
+      out.writeln(mat.rect_sum(a[i], c[i], b[i], d[i]));
+    }
+  }
+}
