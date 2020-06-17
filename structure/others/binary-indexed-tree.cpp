5fa00cb86e35e309c42864c759f198c46cd1bf85
ei13333 <missnakakun@gmail.com>
2020-06-17 14:51:53 +0900

    update bit

--- structure/others/binary-indexed-tree.cpp
+++ structure/others/binary-indexed-tree.cpp
@@ -10,6 +10,14 @@ struct BinaryIndexedTree {
 
   explicit BinaryIndexedTree(size_t sz) : data(sz + 1, 0) {}
 
+  explicit BinaryIndexedTree(const vector< T > &vs) : data(vs.size() + 1, 0) {
+    for(size_t i = 0; i < vs.size(); i++) data[i + 1] = vs[i];
+    for(size_t i = 1; i < data.size(); i++) {
+      size_t j = i + (i & -i);
+      if(j < data.size()) data[j] += data[i];
+    }
+  }
+
   void add(int k, const T &x) {
     for(++k; k < (int) data.size(); k += k & -k) data[k] += x;
   }
