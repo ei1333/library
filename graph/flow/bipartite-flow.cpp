19b11702446ebd2a8cf872b32909a374111f453f
ei13333 <missnakakun@gmail.com>
2020-08-09 02:21:24 +0900

    fix

--- graph/flow/bipartite-flow.cpp
+++ graph/flow/bipartite-flow.cpp
@@ -6,6 +6,7 @@ struct BipartiteFlow {
   vector< vector< int > > graph;
   vector< int > match_l, match_r, dist, used, alive;
 
+public:
   explicit BipartiteFlow(size_t n, size_t m) :
       n(n), m(m), graph(n), match_l(n, -1), match_r(m, -1), used(n), alive(n, 1), time_stamp(0) {}
 
@@ -13,6 +14,95 @@ struct BipartiteFlow {
     graph[u].push_back(v);
   }
 
+  vector< pair< int, int > > max_matching() {
+    for(;;) {
+      build_augment_path();
+      ++time_stamp;
+      int flow = 0;
+      for(int i = 0; i < n; i++) {
+        if(match_l[i] == -1) flow += find_min_dist_augment_path(i);
+      }
+      if(flow == 0) break;
+    }
+    vector< pair< int, int > > ret;
+    for(int i = 0; i < n; i++) {
+      if(match_l[i] >= 0) ret.emplace_back(i, match_l[i]);
+    }
+    return ret;
+  }
+
+  vector< pair< int, int > > lex_min_max_matching() {
+    max_matching();
+    for(auto &vs : graph) sort(begin(vs), end(vs));
+    vector< pair< int, int > > es;
+    for(int i = 0; i < n; i++) {
+      if(match_l[i] == -1 || alive[i] == 0) {
+        continue;
+      }
+      match_r[match_l[i]] = -1;
+      match_l[i] = -1;
+      ++time_stamp;
+      find_augment_path(i);
+      alive[i] = 0;
+      es.emplace_back(i, match_l[i]);
+    }
+    return es;
+  }
+
+  vector< int > min_vertex_cover() {
+    auto color = get_color();
+    vector< int > ret;
+    for(int i = 0; i < n; i++) {
+      if(!color.first[i]) ret.emplace_back(i);
+    }
+    for(int i = 0; i < m; i++) {
+      if(color.second[i]) ret.emplace_back(i + n);
+    }
+    return ret;
+  }
+
+  vector< int > max_independent_set() {
+    auto color = get_color();
+    vector< int > ret;
+    for(int i = 0; i < n; i++) {
+      if(color.first[i]) ret.emplace_back(i);
+    }
+    for(int i = 0; i < m; i++) {
+      if(!color.second[i]) ret.emplace_back(i + n);
+    }
+    return ret;
+  }
+
+  vector< pair< int, int > > min_edge_cover() {
+    auto es = max_matching();
+    vector< int > need(m);
+    for(int i = 0; i < m; i++) {
+      if(match_r[i] == -1) {
+        need[i] = 1;
+      }
+    }
+    for(int i = 0; i < n; i++) {
+      for(auto &to : graph[i]) {
+        if(need[to] == 1) {
+          need[to] = 0;
+          es.emplace_back(i, to);
+        }
+      }
+      if(match_l[i] >= 0) {
+        continue;
+      }
+      if(graph[i].empty()) {
+        return {};
+      }
+      es.emplace_back(i, graph[i][0]);
+    }
+    for(int i = 0; i < m; i++) {
+      if(need[i] == 1) return {};
+    }
+    return es;
+  }
+
+private:
   void build_augment_path() {
     queue< int > que;
     dist.assign(graph.size(), -1);
@@ -61,39 +151,28 @@ struct BipartiteFlow {
     return false;
   }
 
-
-  vector< pair< int, int > > max_matching() {
-    for(;;) {
-      build_augment_path();
-      ++time_stamp;
-      int flow = 0;
-      for(int i = 0; i < n; i++) {
-        if(match_l[i] == -1) flow += find_min_dist_augment_path(i);
-      }
-      if(flow == 0) break;
-    }
-    vector< pair< int, int > > ret;
-    for(int i = 0; i < n; i++) {
-      if(match_l[i] >= 0) ret.emplace_back(i, match_l[i]);
-    }
-    return ret;
-  }
-
-  vector< pair< int, int > > lex_min_max_matching() {
+  pair< vector< int >, vector< int > > get_color() {
     max_matching();
-    for(auto &vs : graph) sort(begin(vs), end(vs));
-    vector< pair< int, int > > es;
+    queue< int > que;
+    vector< int > color_l(n), color_r(m);
     for(int i = 0; i < n; i++) {
-      if(match_l[i] == -1 || alive[i] == 0) {
-        continue;
+      if(match_l[i] == -1) {
+        color_l[i] = true;
+        que.emplace(i);
       }
-      match_r[match_l[i]] = -1;
-      match_l[i] = -1;
-      ++time_stamp;
-      find_augment_path(i);
-      alive[i] = 0;
-      es.emplace_back(i, match_l[i]);
     }
-    return es;
+    while(!que.empty()) {
+      int a = que.front();
+      que.pop();
+      for(auto &b : graph[a]) {
+        if(match_l[a] == b || color_r[b]) continue;
+        color_r[b] = true;
+        if(match_r[b] >= 0 && !color_l[match_r[b]]) {
+          color_l[match_r[b]] = true;
+          que.emplace(match_r[b]);
+        }
+      }
+    }
+    return {color_l, color_r};
   }
 };
