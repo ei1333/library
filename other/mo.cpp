4a75a8d0bb4f459376fca1b9cb8b9275cd93d530
ei13333 <missnakakun@gmail.com>
2020-08-12 02:55:35 +0900

    add mo

--- other/mo.cpp
+++ other/mo.cpp
@@ -1,41 +1,39 @@
+/**
+ * @brief Mo's Algorithm
+ */
 struct Mo {
-  using ADD = function< void(int) >;
-  using DEL = function< void(int) >;
-  using REM = function< void(int) >;
+  int n;
+  vector< pair< int, int > > lr;
 
-  int width;
-  vector< int > left, right, order;
-  vector< bool > v;
-
-  Mo(int N, int Q) : width((int) sqrt(N)), order(Q), v(N) {
-    iota(begin(order), end(order), 0);
-  }
+  explicit Mo(int n) : n(n) {}
 
   void add(int l, int r) { /* [l, r) */
-    left.emplace_back(l);
-    right.emplace_back(r);
+    lr.emplace_back(l, r);
   }
 
-  int run(const ADD &add, const DEL &del, const REM &rem) {
-    assert(left.size() == order.size());
-    sort(begin(order), end(order), [&](int a, int b) {
-      int ablock = left[a] / width, bblock = left[b] / width;
+  template< typename AL, typename AR, typename EL, typename ER, typename O >
+  void build(const AL &add_left, const AR &add_right, const EL &erase_left, const ER &erase_right, const O &out) {
+    int q = (int) lr.size();
+    int bs = n / min< int >(n, sqrt(q));
+    vector< int > ord(q);
+    iota(begin(ord), end(ord), 0);
+    sort(begin(ord), end(ord), [&](int a, int b) {
+      int ablock = lr[a].first / bs, bblock = lr[b].first / bs;
       if(ablock != bblock) return ablock < bblock;
-      if(ablock & 1) return right[a] < right[b];
-      return right[a] > right[b];
+      return (ablock & 1) ? lr[a].second > lr[b].second : lr[a].second < lr[b].second;
     });
-    int nl = 0, nr = 0;
-    auto push = [&](int idx) {
-      v[idx].flip();
-      if(v[idx]) add(idx);
-      else del(idx);
-    };
-    for(auto idx : order) {
-      while(nl > left[idx]) push(--nl);
-      while(nr < right[idx]) push(nr++);
-      while(nl < left[idx]) push(nl++);
-      while(nr > right[idx]) push(--nr);
-      rem(idx);
+    int l = 0, r = 0;
+    for(auto idx : ord) {
+      while(l > lr[idx].first) add_left(--l);
+      while(r < lr[idx].second) add_right(r++);
+      while(l < lr[idx].first) erase_left(l++);
+      while(r > lr[idx].second) erase_right(--r);
+      out(idx);
     }
+  }
+
+  template< typename A, typename E, typename O >
+  void build(const A &add, const E &erase, const O &out) {
+    build(add, add, erase, erase, out);
   }
 };
---
+++ test/verify/yosupo-static-range-inversions-query.test.cpp
@@ -0,0 +1,51 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_inversions_query"
+
+#include "../../template/template.cpp"
+
+#include "../../other/mo.cpp"
+#include "../../structure/others/binary-indexed-tree.cpp"
+
+int main() {
+  int N, Q;
+  cin >> N >> Q;
+  vector< int > A(N);
+  for(auto &a : A) cin >> a;
+  Mo mo(N);
+  for(int i = 0; i < Q; i++) {
+    int l, r;
+    cin >> l >> r;
+    mo.add(l, r);
+  }
+  vector< int > xs{A};
+  sort(begin(xs), end(xs));
+  xs.erase(unique(begin(xs), end(xs)), end(xs));
+  for(auto &a : A) a = lower_bound(begin(xs), end(xs), a) - begin(xs);
+  BinaryIndexedTree< int > bit(xs.size());
+  int64_t inv = 0, all = 0;
+  vector< int64_t > ans(Q);
+  auto add_left = [&](int idx) {
+    inv += bit.query(A[idx] - 1);
+    bit.add(A[idx], 1);
+    all++;
+  };
+  auto add_right = [&](int idx) {
+    inv += all - bit.query(A[idx]);
+    bit.add(A[idx], 1);
+    ++all;
+  };
+  auto erase_left = [&](int idx) {
+    inv -= bit.query(A[idx] - 1);
+    bit.add(A[idx], -1);
+    --all;
+  };
+  auto erase_right = [&](int idx) {
+    inv -= all - bit.query(A[idx]);
+    bit.add(A[idx], -1);
+    --all;
+  };
+  auto out = [&](int idx) {
+    ans[idx] = inv;
+  };
+  mo.build(add_left, add_right, erase_left, erase_right, out);
+  for(auto &p : ans) cout << p << "\n";
+}
