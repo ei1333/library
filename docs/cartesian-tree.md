882ac0083c8ff080706b506150cd2805949396f5
ei13333 <missnakakun@gmail.com>
2020-09-30 01:59:52 +0900

    add cartesian tree

---
+++ docs/cartesian-tree.md
@@ -0,0 +1,11 @@
+## 概要
+
+数列に対応する Cartesian tree を求める.
+
+## 使い方
+
+* `cartesian_tree(v)`: 数列 `v` に対応する Cartesian tree を返す. 各要素にはその要素の親の idx が格納される. ただし根は `-1`.
+
+## 計算量
+
+* $O(N)$
---
+++ test/verify/yosupo-cartesian-tree.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/cartesian_tree"
+
+#include "../../template/template.cpp"
+
+#include "../../graph/others/cartesian-tree.cpp"
+
+int main() {
+  int N;
+  cin >> N;
+  vector< int > A(N);
+  for(auto &a : A) cin >> a;
+  auto p = cartesian_tree(A);
+  for(int i = 0; i < N; i++) {
+    if(p[i] >= 0) cout << p[i] << " ";
+    else cout << i << " ";
+  }
+  cout << "\n";
+}
