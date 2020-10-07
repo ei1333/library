de73c460b2cda980bf74a9c581223666956eb347
ei13333 <missnakakun@gmail.com>
2020-10-08 01:50:06 +0900

    add prime count

---
+++ test/verify/yosupo-counting-primes.test.cpp
@@ -0,0 +1,14 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/counting_primes"
+
+#include "../../template/template.cpp"
+
+#include "../../math/number-theory/kth-root.cpp"
+#include "../../math/number-theory/prime-table.cpp"
+#include "../../math/number-theory/prime-count.cpp"
+
+int main() {
+  int64_t n;
+  cin >> n;
+  PrimeCount<> pc;
+  cout << pc.pi(n) << "\n";
+}
