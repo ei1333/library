dde444aac312f5e70377e0774d984b404cd3803d
ei13333 <missnakakun@gmail.com>
2020-11-02 01:28:31 +0900

    add lct

---
+++ structure/develop/diameter.cpp
@@ -0,0 +1,82 @@
+template< typename T >
+struct Diameter {
+
+  using key_t = pair< bool, T >;
+  static const T inf = numeric_limits< T >::max() / 3;
+
+  struct light_t {
+    T val, dia;
+
+    T top1, top2;
+    T diameter;
+
+    light_t() : top1(-inf), top2(-inf), diameter(-inf) {}
+
+    void update(const light_t &x, const light_t &y) {
+
+      {
+        top1 = val;
+        top2 = -inf;
+        diameter = dia;
+      }
+
+      update(x);
+      update(y);
+    }
+
+    void update(const light_t &p) {
+      if(top1 < p.top1) {
+        top2 = top1;
+        top1 = p.top1;
+        if(top2 < p.top2) {
+          top2 = p.top2;
+        }
+      } else if(top2 < p.top1) {
+        top2 = p.top1;
+      }
+      if(diameter < p.diameter) {
+        diameter = p.diameter;
+      }
+    }
+  };
+
+  struct heavy_t {
+    bool vertex;
+    T key;
+
+    T all, p_len, c_len, diameter;
+
+    heavy_t() : all(0), p_len(-inf), c_len(-inf), diameter(-inf) {}
+
+    void set_key(const key_t &x) {
+      vertex = x.first;
+      key = x.second;
+    }
+
+    void update(const heavy_t &p, const heavy_t &c, const light_t &l) {
+      if(vertex) {
+        all = p.all + c.all;
+        p_len = max({c.p_len, max(l.top1, p.p_len) + c.all, -inf});
+        c_len = max({p.c_len, max(l.top1, c.c_len) + p.all, -inf});
+        diameter = max({p.diameter, c.diameter, p.p_len + max(l.top1, c.c_len), c.c_len + l.top1, l.top1 + l.top2, l.diameter, -inf});
+        if(key) {
+          p_len = max(p_len, c.all);
+          c_len = max(c_len, p.all);
+          diameter = max({diameter, p.p_len, c.c_len, l.top1, T(0)});
+        }
+      } else {
+        all = p.all + c.all + key;
+        p_len = max({c.p_len, max(l.top1, p.p_len) + key + c.all, -inf});
+        c_len = max({p.c_len, max(l.top1, c.c_len) + key + p.all, -inf});
+        diameter = max({p.diameter, c.diameter, p.p_len + max(l.top1, c.c_len) + key, c.c_len + l.top1 + key, l.top1 + l.top2 + key, l.diameter, -inf});
+      }
+    }
+
+    light_t add() const {
+      light_t x;
+      x.val = c_len;
+      x.dia = diameter;
+      return x;
+    }
+  };
+};
---
+++ structure/develop/splay-tree.cpp
@@ -0,0 +1,117 @@
+template< typename key_t, size_t V >
+struct SplayTree {
+
+  const key_t e;
+
+  SplayTree() : pool(), e(key_t()) {}
+
+  struct Node {
+    Node *l, *r, *p;
+    key_t sum;
+
+    Node() = default;
+
+    explicit Node(const key_t &k) : sum(k), l(nullptr), r(nullptr), p(nullptr) {}
+  };
+
+  ArrayPool< Node, V > pool;
+
+private:
+  void rotr(Node *t) {
+    auto *x = t->p, *y = x->p;
+    if((x->l = t->r)) t->r->p = x;
+    t->r = x, x->p = t;
+    update(x), update(t);
+    if((t->p = y)) {
+      if(y->l == x) y->l = t;
+      if(y->r == x) y->r = t;
+      update(y);
+    }
+  }
+
+  void rotl(Node *t) {
+    auto *x = t->p, *y = x->p;
+    if((x->r = t->l)) t->l->p = x;
+    t->l = x, x->p = t;
+    update(x), update(t);
+    if((t->p = y)) {
+      if(y->l == x) y->l = t;
+      if(y->r == x) y->r = t;
+      update(y);
+    }
+  }
+
+  void update(Node *t) {
+    t->sum.update(sum(t->l), sum(t->r));
+  }
+
+  Node *get_right(Node *t) const {
+    while(t->r) t = t->r;
+    return t;
+  }
+
+  inline Node *alloc(const key_t &v) {
+    auto p = &(*pool.alloc() = Node(v));
+    update(p);
+    return p;
+  }
+
+  void splay(Node *t) {
+    while(t->p) {
+      auto *q = t->p;
+      if(!q->p) {
+        if(q->l == t) rotr(t);
+        else rotl(t);
+      } else {
+        auto *r = q->p;
+        if(r->l == q) {
+          if(q->l == t) rotr(q), rotr(t);
+          else rotl(t), rotr(t);
+        } else {
+          if(q->r == t) rotl(q), rotl(t);
+          else rotr(t), rotl(t);
+        }
+      }
+    }
+  }
+
+public:
+  inline const key_t &sum(Node *t) {
+    return t ? t->sum : e;
+  }
+
+  Node *push_back(Node *t, const key_t &v) {
+    if(!t) {
+      t = alloc(v);
+      return t;
+    } else {
+      Node *cur = get_right(t), *z = alloc(v);
+      z->p = cur;
+      cur->r = z;
+      update(cur);
+      splay(z);
+      return z;
+    }
+  }
+
+  Node *erase(Node *t) {
+    splay(t);
+    Node *x = t->l, *y = t->r;
+    pool.free(t);
+    if(!x) {
+      t = y;
+      if(t) t->p = nullptr;
+    } else if(!y) {
+      t = x;
+      t->p = nullptr;
+    } else {
+      x->p = nullptr;
+      t = get_right(x);
+      splay(t);
+      t->r = y;
+      y->p = t;
+      update(t);
+    }
+    return t;
+  }
+};
