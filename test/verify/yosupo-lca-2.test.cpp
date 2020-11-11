ccd20100f901704951f1a0867c4d608b9f15f003
ei13333 <missnakakun@gmail.com>
2020-11-11 23:28:00 +0900

    add lca doc

---
+++ test/verify/yosupo-lca-2.test.cpp
@@ -0,0 +1,30 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/lca"
+
+#include "../../template/template.cpp"
+
+#include "../../graph/graph-template.cpp"
+
+#include "../../graph/tree/rmq-lowest-common-ancestor.cpp"
+
+#include "../../other/scanner.cpp"
+#include "../../other/printer.cpp"
+
+
+int main() {
+  Scanner in(stdin);
+  Printer out(stdout);
+  int N, Q;
+  in.read(N, Q);
+  RMQLowestCommonAncestor< int > g(N);
+  for(int i = 1; i < N; i++) {
+    int x;
+    in.read(x);
+    g.add_directed_edge(x, i);
+  }
+  g.build();
+  for(int i = 0; i < Q; i++) {
+    int u, v;
+    in.read(u, v);
+    out.writeln(g.lca(u, v));
+  }
+}
