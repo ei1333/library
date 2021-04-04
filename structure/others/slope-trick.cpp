0ad65883113c0953fb9746c6b45554af05f1a9c8
ei13333 <missnakakun@gmail.com>
2021-04-05 00:46:21 +0900

    fix

--- structure/others/slope-trick.cpp
+++ structure/others/slope-trick.cpp
@@ -10,9 +10,40 @@ struct SlopeTrick {
   T min_f;
   priority_queue< T, vector< T >, less<> > L;
   priority_queue< T, vector< T >, greater<> > R;
+  T add_l, add_r;
 
 
-  SlopeTrick() : min_f(0) {
+private:
+  void push_R(const T &a) {
+    R.push(a - add_r);
+  }
+
+  T top_R() const {
+    return R.top() + add_r;
+  }
+
+  T pop_R() {
+    T val = top_R();
+    R.pop();
+    return val;
+  }
+
+  void push_L(const T &a) {
+    L.push(a - add_l);
+  }
+
+  T top_L() const {
+    return L.top() + add_l;
+  }
+
+  T pop_L() {
+    T val = top_L();
+    L.pop();
+    return val;
+  }
+
+public:
+  SlopeTrick() : min_f(0), add_l(0), add_r(0) {
     L.push(-INF);
     R.push(INF);
   }
@@ -23,7 +54,7 @@ struct SlopeTrick {
 
   // return min f(x)
   Query query() const {
-    return (Query) {L.top(), R.top(), min_f};
+    return (Query) {top_L(), top_R(), min_f};
   }
 
   // f(x) += a
@@ -34,19 +65,17 @@ struct SlopeTrick {
   // add \_
   // f(x) += max(a - x, 0)
   void add_a_minus_x(const T &a) {
-    min_f += max(T(0), a - R.top());
-    R.push(a);
-    L.push(R.top());
-    R.pop();
+    min_f += max(T(0), a - top_R());
+    push_R(a);
+    push_L(pop_R());
   }
 
   // add _/
   // f(x) += max(x - a, 0)
   void add_x_minus_a(const T &a) {
-    min_f += max(T(0), L.top() - a);
-    L.push(a);
-    R.push(L.top());
-    L.pop();
+    min_f += max(T(0), top_L() - a);
+    push_L(a);
+    push_R(pop_L());
   }
 
   // add \/
@@ -66,5 +95,24 @@ struct SlopeTrick {
   // f_{new} (x) = min f(y) (y >= x)
   void clear_left() {
     while(L.size() >= 2) L.pop();
+  }
+
+  // \/. -> .\/
+  // f_{new} (x) = f(x - a)
+  void shift(const T &a) {
+    add_l += a;
+    add_r += a;
+  }
+
+  // L, R を破壊する
+  T get(const T &x) {
+    T ret = min_f;
+    while(!L.empty()) {
+      ret += max(T(0), pop_L() - x);
+    }
+    while(!R.empty()) {
+      ret += max(T(0), x - pop_R());
+    }
+    return ret;
   }
 };
