7ae4b3e7df5c731197c155277583fd35dfa4051d
ei13333 <missnakakun@gmail.com>
2021-06-23 17:44:06 +0900

    fix ntt

--- math/fft/number-theoretic-transform-friendly-mod-int.cpp
+++ math/fft/number-theoretic-transform-friendly-mod-int.cpp
@@ -1,28 +1,36 @@
+/**
+ * @brief Number-Theoretic-Transform-Friendly-Mod-Int
+ */
 template< typename Mint >
 struct NumberTheoreticTransformFriendlyModInt {
 
-  vector< Mint > dw, idw;
-  int max_base;
-  Mint root;
+  static vector< Mint > dw, idw;
+  static int max_base;
+  static Mint root;
 
-  NumberTheoreticTransformFriendlyModInt() {
-    const unsigned mod = Mint::get_mod();
-    assert(mod >= 3 && mod % 2 == 1);
-    auto tmp = mod - 1;
-    max_base = 0;
-    while(tmp % 2 == 0) tmp >>= 1, max_base++;
-    root = 2;
-    while(root.pow((mod - 1) >> 1) == 1) root += 1;
-    assert(root.pow(mod - 1) == 1);
-    dw.resize(max_base);
-    idw.resize(max_base);
-    for(int i = 0; i < max_base; i++) {
-      dw[i] = -root.pow((mod - 1) >> (i + 2));
-      idw[i] = Mint(1) / dw[i];
+  NumberTheoreticTransformFriendlyModInt() = default;
+
+  static void init() {
+    if(dw.empty()) {
+      const unsigned mod = Mint::get_mod();
+      assert(mod >= 3 && mod % 2 == 1);
+      auto tmp = mod - 1;
+      max_base = 0;
+      while(tmp % 2 == 0) tmp >>= 1, max_base++;
+      root = 2;
+      while(root.pow((mod - 1) >> 1) == 1) root += 1;
+      assert(root.pow(mod - 1) == 1);
+      dw.resize(max_base);
+      idw.resize(max_base);
+      for(int i = 0; i < max_base; i++) {
+        dw[i] = -root.pow((mod - 1) >> (i + 2));
+        idw[i] = Mint(1) / dw[i];
+      }
     }
   }
 
-  void ntt(vector< Mint > &a) {
+  static void ntt(vector< Mint > &a) {
+    init();
     const int n = (int) a.size();
     assert((n & (n - 1)) == 0);
     assert(__builtin_ctz(n) <= max_base);
@@ -38,7 +46,8 @@ struct NumberTheoreticTransformFriendlyModInt {
     }
   }
 
-  void intt(vector< Mint > &a, bool f = true) {
+  static void intt(vector< Mint > &a, bool f = true) {
+    init();
     const int n = (int) a.size();
     assert((n & (n - 1)) == 0);
     assert(__builtin_ctz(n) <= max_base);
@@ -58,7 +67,7 @@ struct NumberTheoreticTransformFriendlyModInt {
     }
   }
 
-  vector< Mint > multiply(vector< Mint > a, vector< Mint > b) {
+  static vector< Mint > multiply(vector< Mint > a, vector< Mint > b) {
     int need = a.size() + b.size() - 1;
     int nbase = 1;
     while((1 << nbase) < need) nbase++;
@@ -74,3 +83,12 @@ struct NumberTheoreticTransformFriendlyModInt {
     return a;
   }
 };
+
+template< typename Mint >
+vector< Mint > NumberTheoreticTransformFriendlyModInt< Mint >::dw = vector< Mint >();
+template< typename Mint >
+vector< Mint > NumberTheoreticTransformFriendlyModInt< Mint >::idw = vector< Mint >();
+template< typename Mint >
+int NumberTheoreticTransformFriendlyModInt< Mint >::max_base = 0;
+template< typename Mint >
+Mint NumberTheoreticTransformFriendlyModInt< Mint >::root = Mint();
