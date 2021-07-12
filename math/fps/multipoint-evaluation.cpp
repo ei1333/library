6c42c85097e3e367586fb7b3e58345b74ac7b99e
ei13333 <missnakakun@gmail.com>
2021-07-13 00:02:55 +0900

    fix fps

--- math/fps/multipoint-evaluation.cpp
+++ math/fps/multipoint-evaluation.cpp
@@ -1,14 +1,12 @@
+#include "subproduct-tree.cpp"
+
 /**
  * @brief Multipoint-Evaluation
  */
 template< template< typename > class FPS, typename Mint >
 FPS< Mint > multipoint_evaluation(const FPS< Mint > &f, const FPS< Mint > &xs) {
-  int n = (int) f.size(), m = (int) xs.size();
-  int k = 1;
-  while(k < m) k <<= 1;
-  vector< FPS< Mint > > g(2 * k, {1});
-  for(int i = 0; i < m; i++) g[k + i] = {-xs[i], Mint(1)};
-  for(int i = k; i-- > 1;) g[i] = g[i << 1] * g[i << 1 | 1];
+  auto g = subproduct_tree(xs);
+  int m = (int) xs.size(), k = (int) g.size() / 2;
   g[1] = f % g[1];
   for(int i = 2; i < k + m; i++) g[i] = g[i >> 1] % g[i];
   FPS< Mint > ys(m);
--- math/fps/polynomial-interpolation.cpp
+++ math/fps/polynomial-interpolation.cpp
@@ -1,15 +1,14 @@
+#include "subproduct-tree.cpp"
+
 /**
  * @brief Polynomial-Interpolation(多項式補間)
  */
 template< template< typename > class FPS, typename Mint >
 FPS< Mint > polynomial_interpolation(const FPS< Mint > &xs, const FPS< Mint > &ys) {
   assert(xs.size() == ys.size());
-  int n = (int) xs.size();
-  int k = 1;
-  while(k < n) k <<= 1;
-  vector< FPS< Mint > > mul(2 * k, {1}), g(2 * k);
-  for(int i = 0; i < n; i++) mul[k + i] = {-xs[i], Mint(1)};
-  for(int i = k; i-- > 1;) mul[i] = mul[i << 1] * mul[i << 1 | 1];
+  auto mul = subproduct_tree(xs);
+  int n = (int) xs.size(), k = (int) mul.size() / 2;
+  vector< FPS< Mint > > g(2 * k);
   g[1] = mul[1].diff() % mul[1];
   for(int i = 2; i < k + n; i++) g[i] = g[i >> 1] % mul[i];
   for(int i = 0; i < n; i++) g[k + i] = {ys[i] / g[k + i][0]};
