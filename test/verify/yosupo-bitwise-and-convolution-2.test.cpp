8c357ac6165ace7d0caaa463a3c8d521cba95f90
ei13333 <missnakakun@gmail.com>
2021-08-02 23:35:50 +0900

    add or conv

--- test/verify/yosupo-bitwise-and-convolution-2.test.cpp
+++ test/verify/yosupo-bitwise-and-convolution-2.test.cpp
@@ -2,25 +2,38 @@
 
 #include "../../template/template.cpp"
 
-#include "../../math/fft/superset-zeta-moebius-transform-simd.cpp"
+#include "../../math/fft/bitwise-or-convolution.cpp"
+#include "../../math/combinatorics/mod-int.cpp"
 
 #include "../../other/scanner.cpp"
 #include "../../other/printer.cpp"
 
 const int MOD = 998244353;
+using mint = ModInt< MOD >;
 
 int main() {
   Scanner in(stdin);
   Printer out(stdout);
   int N;
   in.read(N);
-  int a[1 << N], b[1 << N];
-  for(int i = 0; i < (1 << N); i++) in.read(a[i]);
-  for(int i = 0; i < (1 << N); i++) in.read(b[i]);
-  bitwise_and_convolution_simd< MOD >(a, b, 1 << N);
+  vector< mint > a(1 << N), b(1 << N);
+  for(auto &x : a) {
+    int p;
+    in.read(p);
+    x = p;
+  }
+  for(auto &x : b) {
+    int p;
+    in.read(p);
+    x = p;
+  }
+  reverse(begin(a), end(a));
+  reverse(begin(b), end(b));
+  auto c = bitwise_or_convolution(a, b);
+  reverse(begin(c), end(c));
   for(int i = 0; i < (1 << N); i++) {
     if(i) out.write(' ');
-    out.write(a[i]);
+    out.write(c[i].x);
   }
   out.writeln();
 }
