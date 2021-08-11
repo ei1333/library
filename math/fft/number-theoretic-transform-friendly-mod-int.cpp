400adca2ed6c46e910198603656e3bae06a3432d
ei13333 <missnakakun@gmail.com>
2021-08-11 23:14:45 +0900

    fix ntt

--- math/fft/number-theoretic-transform-friendly-mod-int.cpp
+++ math/fft/number-theoretic-transform-friendly-mod-int.cpp
@@ -1,30 +1,57 @@
 /**
- * @brief Number Theoretic Transform Friendly Mod Int
+ * @brief Number Theoretic Transform Friendly ModInt
  */
 template< typename Mint >
 struct NumberTheoreticTransformFriendlyModInt {
 
-  static vector< Mint > dw, idw;
+  static vector< Mint > roots, iroots, rate2, rate3, irate2, irate3;
   static int max_base;
-  static Mint root;
 
   NumberTheoreticTransformFriendlyModInt() = default;
 
   static void init() {
-    if(dw.empty()) {
+    if(roots.empty()) {
       const unsigned mod = Mint::get_mod();
       assert(mod >= 3 && mod % 2 == 1);
       auto tmp = mod - 1;
       max_base = 0;
       while(tmp % 2 == 0) tmp >>= 1, max_base++;
-      root = 2;
-      while(root.pow((mod - 1) >> 1) == 1) root += 1;
+      Mint root = 2;
+      while(root.pow((mod - 1) >> 1) == 1) {
+        root += 1;
+      }
       assert(root.pow(mod - 1) == 1);
-      dw.resize(max_base);
-      idw.resize(max_base);
-      for(int i = 0; i < max_base; i++) {
-        dw[i] = -root.pow((mod - 1) >> (i + 2));
-        idw[i] = Mint(1) / dw[i];
+
+      roots.resize(max_base + 1);
+      iroots.resize(max_base + 1);
+      rate2.resize(max_base + 1);
+      irate2.resize(max_base + 1);
+      rate3.resize(max_base + 1);
+      irate3.resize(max_base + 1);
+
+      roots[max_base] = root.pow((mod - 1) >> max_base);
+      iroots[max_base] = Mint(1) / roots[max_base];
+      for(int i = max_base - 1; i >= 0; i--) {
+        roots[i] = roots[i + 1] * roots[i + 1];
+        iroots[i] = iroots[i + 1] * iroots[i + 1];
+      }
+      {
+        Mint prod = 1, iprod = 1;
+        for(int i = 0; i <= max_base - 2; i++) {
+          rate2[i] = roots[i + 2] * prod;
+          irate2[i] = iroots[i + 2] * iprod;
+          prod *= iroots[i + 2];
+          iprod *= roots[i + 2];
+        }
+      }
+      {
+        Mint prod = 1, iprod = 1;
+        for(int i = 0; i <= max_base - 3; i++) {
+          rate3[i] = roots[i + 3] * prod;
+          irate3[i] = iroots[i + 3] * iprod;
+          prod *= iroots[i + 3];
+          iprod *= roots[i + 3];
+        }
       }
     }
   }
@@ -33,15 +60,58 @@ struct NumberTheoreticTransformFriendlyModInt {
     init();
     const int n = (int) a.size();
     assert((n & (n - 1)) == 0);
-    assert(__builtin_ctz(n) <= max_base);
-    for(int m = n; m >>= 1;) {
-      Mint w = 1;
-      for(int s = 0, k = 0; s < n; s += 2 * m) {
-        for(int i = s, j = s + m; i < s + m; ++i, ++j) {
-          auto x = a[i], y = a[j] * w;
-          a[i] = x + y, a[j] = x - y;
+    int h = __builtin_ctz(n);
+    assert(h <= max_base);
+    int len = 0;
+    Mint imag = roots[2];
+    if(h & 1) {
+      int p = 1 << (h - 1);
+      Mint rot = 1;
+      for(int i = 0; i < p; i++) {
+        auto r = a[i + p];
+        a[i + p] = a[i] - r;
+        a[i] += r;
+      }
+      len++;
+    }
+    for(; len + 1 < h; len += 2) {
+      int p = 1 << (h - len - 2);
+      { // s = 0
+        for(int i = 0; i < p; i++) {
+          auto a0 = a[i];
+          auto a1 = a[i + p];
+          auto a2 = a[i + 2 * p];
+          auto a3 = a[i + 3 * p];
+          auto a1na3imag = (a1 - a3) * imag;
+          auto a0a2 = a0 + a2;
+          auto a1a3 = a1 + a3;
+          auto a0na2 = a0 - a2;
+          a[i] = a0a2 + a1a3;
+          a[i + 1 * p] = a0a2 - a1a3;
+          a[i + 2 * p] = a0na2 + a1na3imag;
+          a[i + 3 * p] = a0na2 - a1na3imag;
         }
-        w *= dw[__builtin_ctz(++k)];
+      }
+      Mint rot = rate3[__builtin_ctz(~0)];
+      for(int s = 1; s < (1 << len); s++) {
+        int offset = s << (h - len);
+        Mint rot2 = rot * rot;
+        Mint rot3 = rot2 * rot;
+        for(int i = 0; i < p; i++) {
+          auto a0 = a[i + offset];
+          auto a1 = a[i + offset + p] * rot;
+          auto a2 = a[i + offset + 2 * p] * rot2;
+          auto a3 = a[i + offset + 3 * p] * rot3;
+          auto a1na3imag = (a1 - a3) * imag;
+          auto a0a2 = a0 + a2;
+          auto a1a3 = a1 + a3;
+          auto a0na2 = a0 - a2;
+          a[i + offset] = a0a2 + a1a3;
+          a[i + offset + 1 * p] = a0a2 - a1a3;
+          a[i + offset + 2 * p] = a0na2 + a1na3imag;
+          a[i + offset + 3 * p] = a0na2 - a1na3imag;
+        }
+        rot *= rate3[__builtin_ctz(~s)];
       }
     }
   }
@@ -50,15 +120,56 @@ struct NumberTheoreticTransformFriendlyModInt {
     init();
     const int n = (int) a.size();
     assert((n & (n - 1)) == 0);
-    assert(__builtin_ctz(n) <= max_base);
-    for(int m = 1; m < n; m *= 2) {
-      Mint w = 1;
-      for(int s = 0, k = 0; s < n; s += 2 * m) {
-        for(int i = s, j = s + m; i < s + m; ++i, ++j) {
-          auto x = a[i], y = a[j];
-          a[i] = x + y, a[j] = (x - y) * w;
+    int h = __builtin_ctz(n);
+    assert(h <= max_base);
+    int len = h;
+    Mint iimag = iroots[2];
+    for(; len > 1; len -= 2) {
+      int p = 1 << (h - len);
+      { // s = 0
+        for(int i = 0; i < p; i++) {
+          auto a0 = a[i];
+          auto a1 = a[i + 1 * p];
+          auto a2 = a[i + 2 * p];
+          auto a3 = a[i + 3 * p];
+          auto a2na3iimag = (a2 - a3) * iimag;
+          auto a0na1 = a0 - a1;
+          auto a0a1 = a0 + a1;
+          auto a2a3 = a2 + a3;
+          a[i] = a0a1 + a2a3;
+          a[i + 1 * p] = (a0na1 + a2na3iimag);
+          a[i + 2 * p] = (a0a1 - a2a3);
+          a[i + 3 * p] = (a0na1 - a2na3iimag);
         }
-        w *= idw[__builtin_ctz(++k)];
+      }
+      Mint irot = irate3[__builtin_ctz(~0)];
+      for(int s = 1; s < (1 << (len - 2)); s++) {
+        int offset = s << (h - len + 2);
+        Mint irot2 = irot * irot;
+        Mint irot3 = irot2 * irot;
+        for(int i = 0; i < p; i++) {
+          auto a0 = a[i + offset];
+          auto a1 = a[i + offset + 1 * p];
+          auto a2 = a[i + offset + 2 * p];
+          auto a3 = a[i + offset + 3 * p];
+          auto a2na3iimag = (a2 - a3) * iimag;
+          auto a0na1 = a0 - a1;
+          auto a0a1 = a0 + a1;
+          auto a2a3 = a2 + a3;
+          a[i + offset] = a0a1 + a2a3;
+          a[i + offset + 1 * p] = (a0na1 + a2na3iimag) * irot;
+          a[i + offset + 2 * p] = (a0a1 - a2a3) * irot2;
+          a[i + offset + 3 * p] = (a0na1 - a2na3iimag) * irot3;
+        }
+        irot *= irate3[__builtin_ctz(~s)];
+      }
+    }
+    if(len >= 1) {
+      int p = 1 << (h - 1);
+      for(int i = 0; i < p; i++) {
+        auto ajp = a[i] - a[i + p];
+        a[i] += a[i + p];
+        a[i + p] = ajp;
       }
     }
     if(f) {
@@ -85,10 +196,16 @@ struct NumberTheoreticTransformFriendlyModInt {
 };
 
 template< typename Mint >
-vector< Mint > NumberTheoreticTransformFriendlyModInt< Mint >::dw = vector< Mint >();
+vector< Mint > NumberTheoreticTransformFriendlyModInt< Mint >::roots = vector< Mint >();
 template< typename Mint >
-vector< Mint > NumberTheoreticTransformFriendlyModInt< Mint >::idw = vector< Mint >();
+vector< Mint > NumberTheoreticTransformFriendlyModInt< Mint >::iroots = vector< Mint >();
+template< typename Mint >
+vector< Mint > NumberTheoreticTransformFriendlyModInt< Mint >::rate2 = vector< Mint >();
+template< typename Mint >
+vector< Mint > NumberTheoreticTransformFriendlyModInt< Mint >::irate2 = vector< Mint >();
+template< typename Mint >
+vector< Mint > NumberTheoreticTransformFriendlyModInt< Mint >::rate3 = vector< Mint >();
+template< typename Mint >
+vector< Mint > NumberTheoreticTransformFriendlyModInt< Mint >::irate3 = vector< Mint >();
 template< typename Mint >
 int NumberTheoreticTransformFriendlyModInt< Mint >::max_base = 0;
-template< typename Mint >
-Mint NumberTheoreticTransformFriendlyModInt< Mint >::root = Mint();
