252fc9f7ad582bc3c5085386738009e04e1ecc1b
ei13333 <missnakakun@gmail.com>
2021-08-25 15:39:13 +0900

    fix knapsack of w[i]==0

--- dp/knapsack-limitations.cpp
+++ dp/knapsack-limitations.cpp
@@ -10,18 +10,26 @@ vector< T > knapsack_limitations(const vector< int > &w, const vector< int > &m,
   dp[0] = T();
   vector< int > deq(W + 1);
   for(int i = 0; i < N; i++) {
-    for(int a = 0; a < w[i]; a++) {
-      int s = 0, t = 0;
-      for(int j = 0; w[i] * j + a <= W; j++) {
-        if(dp[w[i] * j + a] != NG) {
-          auto val = dp[w[i] * j + a] - j * v[i];
-          while(s < t && comp(val, deqv[t - 1])) --t;
-          deq[t] = j;
-          deqv[t++] = val;
+    if(w[i] == 0) {
+      for(int j = 0; j <= W; j++) {
+        if(dp[j] != NG && comp(dp[j] + v[i] * m[i], dp[j])) {
+          dp[j] = dp[j] + v[i] * m[i];
         }
-        if(s < t) {
-          dp[j * w[i] + a] = deqv[s] + j * v[i];
-          if(deq[s] == j - m[i]) ++s;
+      }
+    } else {
+      for(int a = 0; a < w[i]; a++) {
+        int s = 0, t = 0;
+        for(int j = 0; w[i] * j + a <= W; j++) {
+          if(dp[w[i] * j + a] != NG) {
+            auto val = dp[w[i] * j + a] - j * v[i];
+            while(s < t && comp(val, deqv[t - 1])) --t;
+            deq[t] = j;
+            deqv[t++] = val;
+          }
+          if(s < t) {
+            dp[j * w[i] + a] = deqv[s] + j * v[i];
+            if(deq[s] == j - m[i]) ++s;
+          }
         }
       }
     }
