9f882ef98784a5354c4e597f6d6995a2470cbb9e
ei13333 <missnakakun@gmail.com>
2021-08-29 19:33:00 +0900

    fix #34

--- math/fps/multipoint-evaluation.cpp
+++ math/fps/multipoint-evaluation.cpp
@@ -10,6 +10,6 @@ FPS< Mint > multipoint_evaluation(const FPS< Mint > &f, const FPS< Mint > &xs) {
   g[1] = f % g[1];
   for(int i = 2; i < k + m; i++) g[i] = g[i >> 1] % g[i];
   FPS< Mint > ys(m);
-  for(int i = 0; i < m; i++) ys[i] = g[k + i][0];
+  for(int i = 0; i < m; i++) ys[i] = (g[k + i].empty() ? Mint(0) : g[k + i][0]);
   return ys;
 }
