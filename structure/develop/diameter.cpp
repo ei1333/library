72cbda00af5902473d6dbabc8c342d1439a87865
ei13333 <missnakakun@gmail.com>
2021-10-11 00:07:13 +0900

    fix diameter

--- structure/develop/diameter.cpp
+++ structure/develop/diameter.cpp
@@ -1,76 +1,96 @@
-template< typename T >
-struct Diameter {
+#include "super-link-cut-tree.cpp"
 
-  using key_t = pair< bool, T >;
-  static const T inf = numeric_limits< T >::max() / 3;
+/**
+ * @brief Diameter
+ */
+using T = int64_t;
 
-  struct light_t {
-    T val, dia;
+// 遅延伝搬をするための作用素
+struct Lazy {
 
-    T top1, top2;
-    T diameter;
+  // 単位元
+  Lazy() {}
 
-    light_t() : top1(-inf), top2(-inf), diameter(-inf) {}
+  // 初期化
+  Lazy(T v) {}
 
-    void set_sum() {
-      top1 = val;
-      top2 = -inf;
-      diameter = dia;
-    }
-
-    void update(const light_t &p) {
-      if(top1 < p.top1) {
-        top2 = top1;
-        top1 = p.top1;
-        if(top2 < p.top2) {
-          top2 = p.top2;
-        }
-      } else if(top2 < p.top1) {
-        top2 = p.top1;
-      }
-      if(diameter < p.diameter) {
-        diameter = p.diameter;
-      }
-    }
-  };
-
-  struct heavy_t {
-    bool vertex;
-    T key;
-
-    T all, p_len, c_len, diameter;
-
-    heavy_t() : all(0), p_len(-inf), c_len(-inf), diameter(-inf) {}
-
-    void set_key(const key_t &x) {
-      vertex = x.first;
-      key = x.second;
-    }
-
-    void update(const heavy_t &p, const heavy_t &c, const light_t &l) {
-      if(vertex) {
-        all = p.all + c.all;
-        p_len = max({c.p_len, max(l.top1, p.p_len) + c.all, -inf});
-        c_len = max({p.c_len, max(l.top1, c.c_len) + p.all, -inf});
-        diameter = max({p.diameter, c.diameter, p.p_len + max(l.top1, c.c_len), c.c_len + l.top1, l.top1 + l.top2, l.diameter, -inf});
-        if(key) {
-          p_len = max(p_len, c.all);
-          c_len = max(c_len, p.all);
-          diameter = max({diameter, p.p_len, c.c_len, l.top1, T(0)});
-        }
-      } else {
-        all = p.all + c.all + key;
-        p_len = max({c.p_len, max(l.top1, p.p_len) + key + c.all, -inf});
-        c_len = max({p.c_len, max(l.top1, c.c_len) + key + p.all, -inf});
-        diameter = max({p.diameter, c.diameter, p.p_len + max(l.top1, c.c_len) + key, c.c_len + l.top1 + key, l.top1 + l.top2 + key, l.diameter, -inf});
-      }
-    }
-
-    light_t add() const {
-      light_t x;
-      x.val = c_len;
-      x.dia = diameter;
-      return x;
-    }
-  };
+  // 遅延伝搬
+  void propagate(const Lazy &p) {}
 };
+
+// Light-edge の情報
+template< typename Lazy >
+struct LInfo {
+  T dia, dep;
+
+  T dia_max, dep_max, dep_max2;
+
+  // 単位元(キーの値はアクセスしないので未初期化でもよい
+  LInfo() : dia_max(-infll), dep_max(-infll), dep_max2(-infll) {}
+
+  // 初期化
+  LInfo(T dia, T dep) : dia(dia), dep(dep) {}
+
+  // l, r は Splay-tree の子 (原理上、各ノード区別はない)
+  void update(const LInfo &l, const LInfo &r) {
+    dia_max = max({l.dia_max, r.dia_max, dia});
+    if(dep < l.dep_max) {
+      dep_max2 = max(l.dep_max2, dep);
+      dep_max = l.dep_max;
+    } else {
+      dep_max2 = l.dep_max;
+      dep_max = dep;
+    }
+    if(dep_max < r.dep_max) {
+      dep_max2 = max(dep_max, r.dep_max2);
+      dep_max = r.dep_max;
+    } else {
+      dep_max2 = max(dep_max2, r.dep_max);
+    }
+  }
+
+  // 部分木への遅延伝搬
+  void propagate(const Lazy &p) {}
+};
+
+// Heavy-edge の情報
+template< typename LInfo, typename Lazy >
+struct Info {
+  T cost;
+
+  T dia_max, p_len, c_len, all;
+
+  // 単位元(キーの値はアクセスしないので未初期化でもよい
+  Info() : dia_max(0), p_len(0), c_len(0), all(0) {}
+
+  // 初期化
+  Info(T v) : cost(v) {}
+
+  // 反転
+  void toggle() {
+    swap(p_len, c_len);
+  }
+
+  // pが親, cがheavy-edgeで結ばれた子, lがそれ以外の子
+  void update(const Info &p, const Info &c, const LInfo &l) {
+    all = p.all + cost + c.all;
+    p_len = max({p.p_len, p.all + cost + max(l.dep_max, c.p_len)});
+    c_len = max({c.c_len, c.all + cost + max(l.dep_max, p.c_len)});
+    dia_max = max({p.dia_max, c.dia_max, l.dia_max,
+                   l.dep_max + cost + l.dep_max2,
+                   p.c_len + cost + max(l.dep_max, c.p_len),
+                   l.dep_max + cost + c.p_len});
+  }
+
+  // 親と light-edge で繋げる
+  LInfo link() const { return LInfo(dia_max, p_len); }
+
+  // 遅延伝搬
+  void propagate(const Lazy &p) {}
+
+  // light-edgeに対する遅延伝搬
+  // pathとsubtreeの遅延伝搬が両方ある場合に実装する
+  void propagate_light(const Lazy &p) {}
+};
+
+using LCT = SuperLinkCutTree< Info, LInfo, Lazy >;
--- test/verify/aoj-grl-5-a-2.test.cpp
+++ test/verify/aoj-grl-5-a-2.test.cpp
@@ -1,17 +1,11 @@
 #define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_5_A"
 
 #include "../../template/template.cpp"
-
-#include "../../structure/develop/array-pool.cpp"
-
-#include "../../structure/develop/link-cut-tree-subtree.cpp"
-
 #include "../../structure/develop/diameter.cpp"
 
 int main() {
   int N;
   cin >> N;
-  using LCT = LinkCutTreeSubTree< Diameter< int >, 200000 >;
   LCT lct;
   vector< vector< pair< int, int > > > g(N);
   for(int i = 1; i < N; i++) {
@@ -20,18 +14,17 @@ int main() {
     g[x].emplace_back(y, z);
     g[y].emplace_back(x, z);
   }
-  vector< LCT::Node * > vs(N), es(N);
+  vector< LCT::NP > vs(N), es(N);
   auto rec = MFP([&](auto rec, int idx, int par) -> void {
-    vs[idx] = lct.alloc(make_pair(true, 1));
-    for(auto &e : g[idx]) {
+    vs[idx] = lct.alloc(0);
+    for(auto &e: g[idx]) {
       if(e.first == par) continue;
       rec(e.first, idx);
-      es[e.first] = lct.alloc(make_pair(false, e.second));
+      es[e.first] = lct.alloc(e.second);
       lct.link(vs[e.first], es[e.first]);
       lct.link(es[e.first], vs[idx]);
     }
   });
   rec(0, -1);
-  lct.expose(vs[0]);
-  cout << vs[0]->sum.diameter << "\n";
+  cout << lct.query(vs[0]).dia_max << "\n";
 }
