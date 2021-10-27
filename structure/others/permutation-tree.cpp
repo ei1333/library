c6214419b367cdc19fd6a57d1f22902204c0d691
ei13333 <missnakakun@gmail.com>
2021-10-27 17:32:42 +0900

    close #40

---
+++ structure/others/permutation-tree.cpp
@@ -0,0 +1,103 @@
+#include "../segment-tree/lazy-segment-tree.cpp"
+
+/**
+ * @brief Permutation Tree(順列木)
+ * @see https://codeforces.com/blog/entry/78898
+ */
+struct PermutationTree {
+public:
+  enum NodeType {
+    JOIN_ASC,
+    JOIN_DESC,
+    LEAF,
+    CUT
+  };
+
+  struct Node {
+    NodeType type;
+    int l, r; // [l, r)
+    int min_v, max_v; // [min_v, max_v)
+    vector< Node * > ch;
+
+    size_t size() const { return r - l; }
+
+    bool is_join() const { return type == JOIN_ASC or type == JOIN_DESC; };
+
+    bool is_leaf() const { return type == LEAF; }
+
+    bool is_cut() const { return type == CUT; }
+  };
+
+  using NP = Node *;
+
+  PermutationTree() = default;
+
+private:
+  static void add_child(NP t, NP c) {
+    t->ch.emplace_back(c);
+    t->l = min(t->l, c->l);
+    t->r = max(t->r, c->r);
+    t->min_v = min(t->min_v, c->min_v);
+    t->max_v = max(t->max_v, c->max_v);
+  }
+
+public:
+  static NP build(vector< int > &A) {
+    int n = (int) A.size();
+
+    vector< int > desc{-1};
+    vector< int > asc{-1};
+    vector< NP > st;
+
+    auto f = [](int a, int b) { return min(a, b); };
+    auto g = [](int a, int b) { return a + b; };
+    constexpr int lim = (1 << 30) - 1;
+    auto seg = get_lazy_segment_tree(vector< int >(n), f, g, g, lim, 0);
+
+    for(int i = 0; i < n; i++) {
+      while(~desc.back() and A[i] > A[desc.back()]) {
+        seg.apply(desc[desc.size() - 2] + 1, desc.back() + 1, A[i] - A[desc.back()]);
+        desc.pop_back();
+      }
+      while(~asc.back() and A[i] < A[asc.back()]) {
+        seg.apply(asc[asc.size() - 2] + 1, asc.back() + 1, A[asc.back()] - A[i]);
+        asc.pop_back();
+      }
+      desc.emplace_back(i);
+      asc.emplace_back(i);
+
+      NP t = new Node{LEAF, i, i + 1, A[i], A[i] + 1, {}};
+      for(;;) {
+        NodeType type = CUT;
+        if(not st.empty()) {
+          if(st.back()->max_v == t->min_v) {
+            type = JOIN_ASC;
+          } else if(t->max_v == st.back()->min_v) {
+            type = JOIN_DESC;
+          }
+        }
+        if(type != CUT) {
+          NP r = st.back();
+          if(type != r->type) {
+            r = new Node{type, r->l, r->r, r->min_v, r->max_v, {r}};
+          }
+          add_child(r, t);
+          st.pop_back();
+          t = r;
+        } else if(seg.prod(0, i + 1 - (int) t->size()) == 0) {
+          t = new Node{CUT, t->l, t->r, t->min_v, t->max_v, {t}};
+          do {
+            add_child(t, st.back());
+            st.pop_back();
+          } while(t->max_v - t->min_v != t->size());
+          reverse(begin(t->ch), end(t->ch));
+        } else {
+          break;
+        }
+      }
+      st.emplace_back(t);
+      seg.apply(0, i + 1, -1);
+    }
+    return st[0];
+  }
+};
---
+++ test/verify/yukicoder-1720.test.cpp
@@ -0,0 +1,41 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1720"
+
+#include "../../template/template.cpp"
+
+#include "../../structure/others/permutation-tree.cpp"
+
+#include "../../math/combinatorics/montgomery-mod-int.cpp"
+
+const int MOD = 998244353;
+using mint = MontgomeryModInt< MOD >;
+
+int main() {
+  int N, K;
+  cin >> N >> K;
+  vector< int > A(N);
+  cin >> A;
+  for(auto &a: A) --a;
+  using NP = PermutationTree::Node *;
+  auto dp = make_v< mint >(K + 1, N + 1);
+  dp[0][0] = 1;
+  MFP([&](auto rec, NP r) -> void {
+    if(r->is_cut() or r->is_leaf()) {
+      for(int k = 0; k < K; k++) {
+        dp[k + 1][r->r] += dp[k][r->l];
+      }
+    }
+    vector< mint > sum(K);
+    for(auto &c: r->ch) {
+      rec(c);
+      if(r->is_join()) {
+        for(int k = 0; k < K; k++) {
+          dp[k + 1][c->r] += sum[k];
+          sum[k] += dp[k][c->l];
+        }
+      }
+    }
+  })(PermutationTree::build(A));
+  for(int i = 1; i <= K; i++) {
+    cout << dp[i][N] << "\n";
+  }
+}
