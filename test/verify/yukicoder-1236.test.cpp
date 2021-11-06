d54eca020d0b4d7484922f1d44325435fa5cac77
ei13333 <missnakakun@gmail.com>
2021-11-07 01:16:35 +0900

    #45 add bug

---
+++ test/verify/yukicoder-1236.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1236"
+
+#include "../../template/template.cpp"
+
+#include "../../math/rational/rational.hpp"
+
+int main() {
+  string s, t;
+  cin >> s >> t;
+  int a = stoi(s);
+  int b = stoi(t);
+  using F = Rational< int >;
+  F c(a * 60 + b);
+  F d(720, 11);
+  F x(0);
+  while(x < c) x += d;
+  x = x - c;
+  cout << x.numerator() * 60 / x.denominator() << "\n";
+}
