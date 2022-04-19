01c40cb52bef338e4264ee08ae42910f01292770
ei13333 <missnakakun@gmail.com>
2022-04-19 19:56:57 +0900

    add test cht

---
+++ test/verify/yosupo-line-add-get-min-2.test.cpp
@@ -0,0 +1,60 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/line_add_get_min"
+
+#include "../../template/template.cpp"
+
+#include "../../structure/convex-hull-trick/convex-hull-trick-add-monotone.cpp"
+
+int main() {
+  int N, Q;
+  cin >> N >> Q;
+  using CHT = ConvexHullTrickAddMonotone< int64, true >;
+  vector< CHT > cht;
+  auto add = [&](int64 a, int64 b) {
+    cht.emplace_back();
+    cht.back().add(a, b);
+    while(cht.size() >= 2 and cht.back().H.size() >= cht[cht.size() - 2].H.size()) {
+      auto X = cht.back().H;
+      cht.pop_back();
+      auto Y = cht.back().H;
+      cht.pop_back();
+      reverse(begin(X), end(X));
+      reverse(begin(Y), end(Y));
+      CHT c;
+      int k = 0;
+      for(auto &p: X) {
+        while(k < (int) Y.size() and Y[k] < p) {
+          c.add(Y[k].first, Y[k].second);
+          ++k;
+        }
+        c.add(p.first, p.second);
+      }
+      while(k < (int) Y.size()) {
+        c.add(Y[k].first, Y[k].second);
+        ++k;
+      }
+      cht.emplace_back(c);
+    }
+  };
+  for(int i = 0; i < N; i++) {
+    int64 a, b;
+    cin >> a >> b;
+    add(a, b);
+  }
+  while(Q--) {
+    int t;
+    cin >> t;
+    if(t == 0) {
+      int64 a, b;
+      cin >> a >> b;
+      add(a, b);
+    } else {
+      int64 x;
+      cin >> x;
+      int64 ret = infll;
+      for(auto &c: cht) {
+        chmin(ret, c.query(x));
+      }
+      cout << ret << "\n";
+    }
+  }
+}
