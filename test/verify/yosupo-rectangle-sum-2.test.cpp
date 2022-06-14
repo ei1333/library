cc9ca219d41a5914f078c61642a3371ff682b317
ei13333 <missnakakun@gmail.com>
2022-06-14 14:38:43 +0900

    #62 笑

--- test/verify/yosupo-rectangle-sum-2.test.cpp
+++ test/verify/yosupo-rectangle-sum-2.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/point_add_rectangle_sum"
+#define PROBLEM "https://judge.yosupo.jp/problem/rectangle_sum"
 
 #include "../../template/template.cpp"
 
