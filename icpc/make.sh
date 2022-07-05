94fb0eaac52b3d6069e4c15d26612f68ab5b3df4
ei1333 <ei1333hobby@gmail.com>
2022-07-05 22:11:46 +0900

    勇気の削除
    これいらん

--- icpc/make.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python make.py > library.cpp
-enscript --highlight=cpp --line-numbers --color --header='Shizuoka University (team: tapu)' library.cpp -o library.ps
-pstopdf library.ps
