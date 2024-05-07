ae698c85906b8e5ef32dee2cf7c13262585873fd
ei1333 <ei1333hobby@gmail.com>
2024-05-08 01:38:19 +0900

    uku

--- structure/dynamic-tree/vertex-set-path-composite.hpp
+++ structure/dynamic-tree/vertex-set-path-composite.hpp
@@ -4,7 +4,6 @@
 using mint = modint998244353;
 
 struct VertexSetPathComposite {
-  struct Point {};
   struct Path {
     mint a, b;
   };
@@ -13,9 +12,6 @@ struct VertexSetPathComposite {
   };
 
   static Path vertex(const Info& u) { return {u.x, u.y}; }
-  static Path add_vertex(const Point& d, const Info& u) { return {u.x, u.y}; }
-  static Point add_edge(const Path& d) { return {}; }
-  static Point rake(const Point& l, const Point& r) { return {}; }
   static Path compress(const Path& p, const Path& c) {
     return {p.a * c.a, p.b * c.a + c.b};
   }
