6c9f5ecc0222e0bea705ffd58b68547188b61964
ei1333 <ei1333hobby@gmail.com>
2024-09-17 00:31:58 +0900

    apply clang-format

--- math/combinatorics/modint-61-1.hpp
+++ math/combinatorics/modint-61-1.hpp
@@ -23,60 +23,42 @@ struct ModInt61_1 {
   mint &operator*=(const mint &p) {
     u128 c = (u128)x * p.x;
     x = u64(c >> 61) + u64(c & mod());
-    if(x >= mod()) x -= mod();
+    if (x >= mod()) x -= mod();
     return *this;
   }
 
-  mint &operator/=(const mint& p) {
+  mint &operator/=(const mint &p) {
     *this *= p.inv();
     return *this;
   }
 
-  mint operator-() const {
-    return mint() -= *this;
-  }
+  mint operator-() const { return mint() -= *this; }
 
-  mint operator+(const mint& p) {
-    return mint(*this) += p;
-  }
+  mint operator+(const mint &p) { return mint(*this) += p; }
 
-  mint operator-(const mint& p) {
-    return mint(*this) -= p;
-  }
+  mint operator-(const mint &p) { return mint(*this) -= p; }
 
-  mint operator*(const mint &p) const {
-    return mint(*this) *= p;
-  }
+  mint operator*(const mint &p) const { return mint(*this) *= p; }
 
-  mint operator/(const mint &p) const {
-    return mint(*this) /= p;
-  }
+  mint operator/(const mint &p) const { return mint(*this) /= p; }
 
-  bool operator==(const mint &p) const {
-    return x == p.x;
-  }
+  bool operator==(const mint &p) const { return x == p.x; }
 
-  bool operator!=(const mint& p) const {
-    return x != p.x;
-  }
+  bool operator!=(const mint &p) const { return x != p.x; }
 
-  u64 val() const {
-    return x;
-  }
+  u64 val() const { return x; }
 
   mint pow(u64 n) const {
     mint ret(1), mul(*this);
-    while(n > 1) {
-      if(n & 1) ret *= mul;
+    while (n > 1) {
+      if (n & 1) ret *= mul;
       mul *= mul;
       n >>= 1;
     }
     return ret;
   }
 
-  mint inv() const {
-    return pow(mod() - 2);
-  }
+  mint inv() const { return pow(mod() - 2); }
 
   friend ostream &operator<<(ostream &os, const mint &p) {
     return os << p.val();
